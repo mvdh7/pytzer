ffbd8b89248647df2a58017b0962881aa8a09325
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-06-18 11:37:37 +0100

    Formatting

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open('README.md','r') as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="pytzer",
-    version="0.0.1",
-    author="Matthew P. Humphreys",
-    author_email="m.p.humphreys@cantab.net",
-    description="Pitzer model for chemical speciation in natural waters",
+    name         = 'pytzer',
+    version      = '0.0.1',
+    author       = 'Matthew P. Humphreys',
+    author_email = 'm.p.humphreys@cantab.net',
+    description  = 'Pitzer model for chemical speciation in natural waters',
+    url          = 'https://github.com/mvdh7/pytzer',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/mvdh7/pytzer",
+    long_description_content_type='text/markdown',
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ),
-)
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',),)
