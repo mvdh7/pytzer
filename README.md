6354503a1e604155ec2c6921d3d1e70b42a4b1f8
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-06-18 11:33:37 +0100

    New repo creation

---
+++ README.md
@@ -0,0 +1,6 @@
+# pytzer
+
+Pitzer model for chemical speciation in natural waters
+
+Written by Matthew P. Humphreys
+University of East Anglia, Norwich, UK
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pytzer",
+    version="0.0.1",
+    author="Matthew P. Humphreys",
+    author_email="m.p.humphreys@cantab.net",
+    description="Pitzer model for chemical speciation in natural waters",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mvdh7/pytzer",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
