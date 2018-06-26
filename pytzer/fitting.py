011c0864c5966bbc05c66bfae21e795e84e5cd82
Matthew Humphreys <m.humphs@gmail.com>
2018-06-26 16:44:25 +0100

    Added fitting for bC paramaters plus manual estimation of their covariance matrix

--- pytzer/fitting.py
+++ pytzer/fitting.py
@@ -3,11 +3,12 @@ from autograd import elementwise_grad as egrad
 from .constants import b, R
 from . import coeffs, miami
 
+# Define f function
 def fG(T,I,Aosm):
     
     return -4 * Aosm * I * np.log(1 + b*np.sqrt(I)) / b
 
-
+# Excess Gibbs energy - single electrolyte
 def Gex_MX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega):
     
     mC = mCmA[:,0]
@@ -25,25 +26,38 @@ def Gex_MX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega):
     
     return Gex_nRT
 
-
+# LN of activity coefficients - single electrolyte
 ln_acf = egrad(Gex_MX)
 
-
+# Activity coefficients - single electrolyte
 def acf(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega):
     
     return np.exp(ln_acf(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega))
 
+# LN of mean activity coefficient - single electrolyte
+def ln_acf_MX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega,nC,nA):
+    
+    ln_acfs = ln_acf(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega)
+    
+    return (nC * ln_acfs[:,0] + nA * ln_acfs[:,1]) / (nC + nA)
 
+# Mean activity coefficient - single electrolyte
+def acf_MX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega,nC,nA):
+    
+    return np.exp(
+        ln_acf_MX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega,nC,nA))
+
+# Osmotic coefficient derivative function - single electrolyte
 def osmfunc(ww,mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega):
     
     return ww * R * T \
         * Gex_MX(np.array([mCmA[:,0]/ww,mCmA[:,1]/ww]).transpose(),
                  zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega)
 
-
+# Osmotic coefficient derivative - single electrolyte
 osmD = egrad(osmfunc)
 
-
+# Osmotic coefficient - single electrolyte
 def osm(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega):
     
     ww = np.full_like(T,1, dtype='float64')
--- pzfit.py
+++ pzfit.py
@@ -1,4 +1,5 @@
-import autograd.numpy as np
+from autograd import numpy as np
+from scipy import optimize
 import pytzer as pz
 
 m = np.array([.1,.5,1.,2.,3.,4.,5.,6.], dtype='float64')
@@ -12,8 +13,6 @@ mCmA = np.array([m*nC,m*nA]).transpose()
 zC = np.float_(+1)
 zA = np.float_(-1)
 
-ww = np.float_(1)
-
 b0 = np.float_(0.0779)
 b1 = np.float_(0.2689)
 b2 = 0
@@ -24,9 +23,30 @@ alph1 = np.float_(2.0)
 alph2 = -9
 omega = -9
 
+nC = np.float_(1)
+nA = np.float_(1)
+
 ww = np.full_like(T,1.)
 
 tgex = pz.fitting.Gex_MX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega)
 tlaf = pz.fitting.ln_acf(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega)
 tacf = pz.fitting.acf   (mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega)
 tosm = pz.fitting.osm   (mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega)
+tacw = pz.miami.osm2aw  (mCmA,tosm)
+tmaf = pz.fitting.acf_MX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega,nC,nA)
+
+# Do fitting!
+topt = optimize.least_squares(lambda bC: 
+    pz.fitting.acf_MX(mCmA,zC,zA,T,bC[0],bC[1],b2,bC[2],C1,
+                      alph1,alph2,omega,nC,nA) - tmaf,
+    np.float_([0,0,0]), jac='3-point')
+
+b0_fit = topt['x'][0]
+b1_fit = topt['x'][1]
+C0_fit = topt['x'][2]
+
+# Get covariance matrix
+mse = np.mean((pz.fitting.acf_MX(mCmA,zC,zA,T,b0_fit,b1_fit,b2,C0_fit,C1,
+                                 alph1,alph2,omega,nC,nA) - tmaf)**2)
+hess = topt.jac.transpose() @ topt.jac
+bCmx = np.linalg.inv(hess) * mse
