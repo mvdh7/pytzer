0cfdf6032ebf6b96f9d2ab282374f239c7268ef6
Matthew Humphreys <m.humphs@gmail.com>
2018-07-02 11:40:01 +0100

    Simulated test dataset ready for H2SO4 parameter fitting

--- sulfit.py
+++ sulfit.py
@@ -1,75 +1,29 @@
 from autograd import numpy as np
-import pandas as pd
 import pytzer as pz
-from scipy import optimize
-import time
+import pickle
 
+# Import coefficients dict
 cf = pz.cdicts.CRP94
 
-T,tots,ions,idf = pz.io.getIons('CRP94 solver.csv')
+# Load simulated dataset
+with open('sulfit.pkl','rb') as f:
+    T,TSO4,mH = pickle.load(f)
+    
+# Calculate parameters to be fitted
+mHSO4 = 2*TSO4 - mH
+mSO4  = mH - TSO4
 
-# Solve for pH - CRP94 system
-def minifun(mH,tots,ions,T,cf):
-    
-    # Calculate [H+] and ionic speciation
-#    mH = np.vstack(-np.log10(pH))
-    mH = np.vstack(mH)
-    mHSO4 = 2*tots - mH
-    mSO4  = mH - tots
-    
-    # Create molality & ions arrays
-    mols = np.concatenate((mH,mHSO4,mSO4), axis=1)
-    ions = np.array(['H','HSO4','SO4'])
-    
-    # Calculate activity coefficients
-    ln_acfs = pz.model.ln_acfs(mols,ions,T,cf)
-    gH    = np.exp(ln_acfs[:,0])
-    gHSO4 = np.exp(ln_acfs[:,1])
-    gSO4  = np.exp(ln_acfs[:,2])
-
-    # Set up DG equation
-#    DG = np.log(gH*mH.ravel() * gSO4*mSO4.ravel() / (gHSO4*mHSO4.ravel())) \
-#        - np.log(cf.K['HSO4'](T)[0])
-    
-    DG = cf.getKeq(T, mH=mH,gH=gH, mHSO4=mHSO4,gHSO4=gHSO4, 
-                   mSO4=mSO4,gSO4=gSO4)
-    
-    return DG
-
-go = time.time()
-
-mH = np.vstack(np.full_like(T,np.nan))
-for i in range(len(mH)):
-    
-    iT = np.array([T[i]])
-    itots = np.array([tots[i,:]])
-    
-    mH[i] = optimize.least_squares(lambda mH: minifun(mH,itots,ions,iT,cf),
-                                   1.5*itots[0],
-                                   bounds=(itots[0],2*itots[0]),
-                                   method='trf',
-                                   xtol=1e-12)['x']
-    
-print(time.time()-go)
-
-# Get solution - all looking good in test case
-mHSO4 = 2*tots - mH
-mSO4  = mH - tots
-
-alpha = mSO4 / tots
+alpha = mSO4 / TSO4
 
 mols = np.concatenate((mH,mHSO4,mSO4), axis=1)
 ions = np.array(['H', 'HSO4', 'SO4'])
 
 osm = pz.model.osm(mols,ions,T,cf)
-osmST = osm * (mH + mHSO4 + mSO4).ravel() / (3 * tots.ravel())
+osmST = osm * (mH + mHSO4 + mSO4).ravel() / (3 * TSO4.ravel())
 
 acfs = pz.model.acfs(mols,ions,T,cf)
 acfPM = np.cbrt(acfs[:,0]**2 * acfs[:,2] * mH.ravel()**2 * mSO4.ravel() \
-    / (4 * tots.ravel()**3))
+    / (4 * TSO4.ravel()**3))
+    
+# Fit parameters!
 
-data = pd.DataFrame({'T':T, 'TSO4':tots.ravel(), 'alpha':alpha.ravel(),
-                     'osm':osm, 'osmST':osmST,
-                     'mH':mH.ravel(), 'mHSO4':mHSO4.ravel(),
-                     'mSO4':mSO4.ravel(), 'gTSO4':acfPM,
-                     'gH':acfs[:,0], 'gHSO4':acfs[:,1], 'gSO4':acfs[:,2]})
---
+++ sulfit_prep.py
@@ -0,0 +1,96 @@
+from autograd import numpy as np
+import pandas as pd
+import pytzer as pz
+from scipy import optimize
+import pickle, time
+
+cf = pz.cdicts.CRP94
+
+T,tots,ions,idf = pz.io.getIons('CRP94 solver.csv')
+
+# Solve for pH - CRP94 system
+def minifun(mH,tots,ions,T,cf):
+    
+    # Calculate [H+] and ionic speciation
+#    mH = np.vstack(-np.log10(pH))
+    mH = np.vstack(mH)
+    mHSO4 = 2*tots - mH
+    mSO4  = mH - tots
+    
+    # Create molality & ions arrays
+    mols = np.concatenate((mH,mHSO4,mSO4), axis=1)
+    ions = np.array(['H','HSO4','SO4'])
+    
+    # Calculate activity coefficients
+    ln_acfs = pz.model.ln_acfs(mols,ions,T,cf)
+    gH    = np.exp(ln_acfs[:,0])
+    gHSO4 = np.exp(ln_acfs[:,1])
+    gSO4  = np.exp(ln_acfs[:,2])
+
+    # Set up DG equation
+#    DG = np.log(gH*mH.ravel() * gSO4*mSO4.ravel() / (gHSO4*mHSO4.ravel())) \
+#        - np.log(cf.K['HSO4'](T)[0])
+    
+    DG = cf.getKeq(T, mH=mH,gH=gH, mHSO4=mHSO4,gHSO4=gHSO4, 
+                   mSO4=mSO4,gSO4=gSO4)
+    
+    return DG
+
+go = time.time()
+
+mH = np.vstack(np.full_like(T,np.nan))
+for i in range(len(mH)):
+    
+    iT = np.array([T[i]])
+    itots = np.array([tots[i,:]])
+    
+    mH[i] = optimize.least_squares(lambda mH: minifun(mH,itots,ions,iT,cf),
+                                   1.5*itots[0],
+                                   bounds=(itots[0],2*itots[0]),
+                                   method='trf',
+                                   xtol=1e-12)['x']
+    
+print(time.time()-go)
+
+# Get solution - all looking good in test case
+mHSO4 = 2*tots - mH
+mSO4  = mH - tots
+
+alpha = mSO4 / tots
+
+mols = np.concatenate((mH,mHSO4,mSO4), axis=1)
+ions = np.array(['H', 'HSO4', 'SO4'])
+
+osm = pz.model.osm(mols,ions,T,cf)
+osmST = osm * (mH + mHSO4 + mSO4).ravel() / (3 * tots.ravel())
+
+acfs = pz.model.acfs(mols,ions,T,cf)
+acfPM = np.cbrt(acfs[:,0]**2 * acfs[:,2] * mH.ravel()**2 * mSO4.ravel() \
+    / (4 * tots.ravel()**3))
+
+data = pd.DataFrame({'T':T, 'TSO4':tots.ravel(), 'alpha':alpha.ravel(),
+                     'osm':osm, 'osmST':osmST,
+                     'mH':mH.ravel(), 'mHSO4':mHSO4.ravel(),
+                     'mSO4':mSO4.ravel(), 'gTSO4':acfPM,
+                     'gH':acfs[:,0], 'gHSO4':acfs[:,1], 'gSO4':acfs[:,2]})
+
+# Simulate and pickle a dataset for fit testing
+s_TSO4 = np.vstack(np.arange(0.01,np.sqrt(6),0.01, dtype='float64')**2)
+s_T = np.full_like(s_TSO4.ravel(),298.15, dtype='float64')
+
+s_mH = np.full_like(s_TSO4,np.nan)
+for i in range(len(s_mH)):
+    
+    print(i)
+    
+    iT = np.array([s_T[i]])
+    itots = np.array([s_TSO4[i,:]])
+    
+    s_mH[i] = optimize.least_squares(lambda mH: minifun(mH,itots,ions,iT,cf),
+                                     1.5*itots[0],
+                                     bounds=(itots[0],2*itots[0]),
+                                     method='trf',
+                                     xtol=1e-12)['x']
+
+with open('sulfit.pkl','wb') as f:
+    pickle.dump((s_T,s_TSO4,s_mH),f)
