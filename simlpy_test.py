def07a7212da66dd0f5103919443fe0a34b2047e
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-07-13 09:31:03 +0100

    Folder reorganisation and file archiving

--- simlpy_test.py
+++
@@ -1,6 +0,0 @@
-#from autograd import numpy as np
-import pickle
-
-with open('E:\Dropbox\_UEA_MPH\pitzer-spritzer\python\pickles' \
-          + '\simloop_pytzer_bC_NaCl.pkl','rb') as f:
-    bCcv,ele,Ureps = pickle.load(f)
