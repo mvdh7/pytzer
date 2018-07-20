Fixing dimensionality for broadcasting

pytzer/fitting.py
@@ -54,7 +54,8 @@ def acfMX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega,nC,nA):
 def osmfunc(ww,mCmA,zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega):
     
     return ww * R * T \
-        * Gex_MX(np.array([mCmA[:,0]/ww,mCmA[:,1]/ww]).transpose(),
+        * Gex_MX(np.array([mCmA[:,0]/ww.ravel(),
+                           mCmA[:,1]/ww.ravel()]).transpose(),
                  zC,zA,T,b0,b1,b2,C0,C1,alph1,alph2,omega)
 
 # Osmotic coefficient derivative - single electrolyte
@@ -137,8 +138,8 @@ def bC_acfMX(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,acfMX,which_bCs):
     if which_bCs == 'b0b1C0':
         
         topt = optimize.least_squares(lambda bC: 
-            acfMX(mCmA,zC,zA,T,bC[0],bC[1],0,bC[2],0,
-                  alph1,alph2,omega,nC,nA) - acfMX,
+            (acfMX(mCmA,zC,zA,T,bC[0],bC[1],0,bC[2],0,
+                   alph1,alph2,omega,nC,nA) - acfMX).ravel(),
             np.float_([0,0,0]), jac=ojac, loss=oloss, method=omethod)
         
         b0 = topt['x'][0]
@@ -148,8 +149,8 @@ def bC_acfMX(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,acfMX,which_bCs):
     elif which_bCs == 'b0b1C0C1':
         
         topt = optimize.least_squares(lambda bC: 
-            acfMX(mCmA,zC,zA,T,bC[0],bC[1],0,bC[2],bC[3],
-                  alph1,alph2,omega,nC,nA) - acfMX,
+            (acfMX(mCmA,zC,zA,T,bC[0],bC[1],0,bC[2],bC[3],
+                   alph1,alph2,omega,nC,nA) - acfMX).ravel(),
             np.float_([0,0,0,0]), jac=ojac, loss=oloss, method=omethod)
         
         b0 = topt['x'][0]
@@ -160,8 +161,8 @@ def bC_acfMX(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,acfMX,which_bCs):
     else:
         
         topt = optimize.least_squares(lambda bC: 
-            acfMX(mCmA,zC,zA,T,bC[0],bC[1],bC[2],bC[3],bC[4],
-                  alph1,alph2,omega,nC,nA) - acfMX,
+            (acfMX(mCmA,zC,zA,T,bC[0],bC[1],bC[2],bC[3],bC[4],
+                   alph1,alph2,omega,nC,nA) - acfMX).ravel(),
             np.float_([0,0,0,0,0]), jac=ojac, loss=oloss, method=omethod)
         
         b0 = topt['x'][0]
@@ -193,8 +194,8 @@ def bC(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,mtarg,which_bCs,mtype):
     if which_bCs == 'b0b1C0':
         
         topt = optimize.least_squares(lambda bC: 
-            ofunc(mCmA,zC,zA,T,bC[0],bC[1],0,bC[2],0,
-                   alph1,alph2,omega,nC,nA) - mtarg,
+            (ofunc(mCmA,zC,zA,T,bC[0],bC[1],0,bC[2],0,
+                   alph1,alph2,omega,nC,nA) - mtarg).ravel(),
             np.float_([0,0,0]), jac=ojac, loss=oloss, method=omethod)
         
         b0 = topt['x'][0]
@@ -204,8 +205,8 @@ def bC(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,mtarg,which_bCs,mtype):
     elif which_bCs == 'b0b1C0C1':
         
         topt = optimize.least_squares(lambda bC: 
-            ofunc(mCmA,zC,zA,T,bC[0],bC[1],0,bC[2],bC[3],
-                   alph1,alph2,omega,nC,nA) - mtarg,
+            (ofunc(mCmA,zC,zA,T,bC[0],bC[1],0,bC[2],bC[3],
+                   alph1,alph2,omega,nC,nA) - mtarg).ravel(),
             np.float_([0,0,0,0]), jac=ojac, loss=oloss, method=omethod)
         
         b0 = topt['x'][0]
@@ -216,8 +217,8 @@ def bC(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,mtarg,which_bCs,mtype):
     else:
         
         topt = optimize.least_squares(lambda bC: 
-            ofunc(mCmA,zC,zA,T,bC[0],bC[1],bC[2],bC[3],bC[4],
-                   alph1,alph2,omega,nC,nA) - mtarg,
+            (ofunc(mCmA,zC,zA,T,bC[0],bC[1],bC[2],bC[3],bC[4],
+                   alph1,alph2,omega,nC,nA) - mtarg).ravel(),
             np.float_([0,0,0,0,0]), jac=ojac, loss=oloss, method=omethod)
         
         b0 = topt['x'][0]

simloop_pytzer.py
@@ -2,7 +2,8 @@ from autograd import numpy as np
 #from sys import path#, argv
 #if 'E:\Dropbox\_UEA_MPH\pitzer-spritzer\python' not in path:
 #    path.append('E:\\Dropbox\\_UEA_MPH\\pitzer-spritzer\\python')
-import pickle, time
+import pickle
+from time import time
 from multiprocessing import Pool
 import pytzer as pz
 pd2vs = pz.misc.pd2vs
@@ -18,10 +19,14 @@ fcs = {'NaCl' : 'b0b1C0C1',
         'KCl'  : 'b0b1C0'  }
 
 aos = {'NaCl' : np.float_([-9, 2,-9,-9, 2.5]),
-        'KCl'  : np.float_([-9, 2,-9,-9,-9  ])}
+        'KCl' : np.float_([-9, 2,-9,-9,-9  ])}
+
+ionslist = {'NaCl' : np.array(['Na','Cl']),
+            'KCl'  : np.array(['K' ,'Cl'])}
 
 fc = fcs[ele]
 ao = aos[ele]
+ions = ionslist[ele]
 
 # Load FPD dataset
 with open('pickles/simpytz_fpd.pkl','rb') as f:
@@ -56,22 +61,23 @@ T0 = pd2vs(fpdbase.t)
 T1 = pd2vs(fpdbase.t25)
 TR = pd2vs(fpdbase.t25)
 osm = pd2vs(fpdbase.osm25_calc)
-ions = np.array(['Na','Cl'])
 
 def Eopt(rseed=None):
 
     # Seed random numbers
     np.random.seed(rseed)
 
-    Uosm = pz.sim.fpd2(ele,tot,srcs,bs,osm,err_cfs_both,fpd_sys_std,
-                       fpd,nC,nA,ions,T0,T1,TR,cf)
+    Uosm = pz.sim.fpd(ele,tot,srcs,bs,osm,err_cfs_both,fpd_sys_std,
+                      fpd,nC,nA,ions,T0,T1,TR,cf)
 
-#    b0,b1,b2,C0,C1,bCmx,mse \
-#        = pz.fitting.bC(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,Uosm,fc,'osm')
+    b0,b1,b2,C0,C1,bCmx,mse \
+        = pz.fitting.bC(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,Uosm,fc,'osm')
 
     return Uosm#b0,b1,b2,C0,C1
 
+go = time()
 Uosm = Eopt()
+print(time()-go)
 #b0,b1,b2,C0,C1 = Eopt()
 
 #%% Multiprocessing loop