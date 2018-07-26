fd2e245b84de23617398b995f0014ac46def0510
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-07-26 12:07:11 +0100

    Plotting different fits

--- simloop_pytzer.m
+++ simloop_pytzer.m
@@ -1,4 +1,4 @@
-load('pickles/simloop_pytzer_bC_KCl_960.mat');
+load('pickles/simloop_pytzer_bC_KCl_9600.mat')
 
 bC_mean = bC_mean([1 2 4]) ;
 bCo     = bCo    ([1 2 4])';
@@ -9,3 +9,37 @@ varnames = {'\beta_0' '\beta_1' '\itC\rm_0'};
 
 covmx_viz(1,bC_mean,bC_cv ,varnames)
 covmx_viz(2,bCo    ,bCo_cv,varnames)
+
+%%
+load('pickles/simloop_res.mat')
+sqt = sqrt(tot);
+plx = tot;
+
+figure(3); clf
+
+subplot(2,1,1); hold on
+
+    plot(plx,acf,'k')
+    plot(plx,acf+sqrt(Uacf),'k--')
+    plot(plx,acf-sqrt(Uacf),'k--')
+
+    plot(plx,acfo,'r')
+    plot(plx,acfo+sqrt(Uacfo),'r--')
+    plot(plx,acfo-sqrt(Uacfo),'r--')
+
+    grid on
+
+subplot(2,1,2); hold on
+
+    plot(plx,acf-acf,'k')
+    plot(plx,sqrt(Uacf),'k--')
+    plot(plx,sqrt(Uacf),'k--')
+
+    plot(plx,acfo-acf,'r')
+    plot(plx,acfo+sqrt(Uacfo)-acf,'r--')
+    plot(plx,acfo-sqrt(Uacfo)-acf,'r--')
+
+    plot(plx,sqrt(Uacfo),'m--')
+    
+    grid on
+    
