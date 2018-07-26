c6c2acae0505b771a8047c6e70b20dea6810755a
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-07-26 09:17:04 +0100

    Working multiprocessing loop for FPD simulations

--- simloop_pytzer.py
+++ simloop_pytzer.py
@@ -1,87 +1,114 @@
-from autograd import numpy as np
-from sys import argv#, path
-#if 'E:\Dropbox\_UEA_MPH\pitzer-spritzer\python' not in path:
-#    path.append('E:\\Dropbox\\_UEA_MPH\\pitzer-spritzer\\python')
+import numpy  as np
+import pandas as pd
 import pickle
-from time import time
-from multiprocessing import Pool
 import pytzer as pz
 pd2vs = pz.misc.pd2vs
+from multiprocessing import Pool
+from sys             import argv
+from time            import time
 
-# Get electrolyte to analyse and number of repeats from user input
-#ele   =     argv[1]
-#Ureps = int(argv[2])
-ele = 'NaCl'
-Ureps = 1
+# Get input args
+Uele =      argv[1]
+Ureps = int(argv[2])
 
-# Get electrolyte-specific information
-fcs = {'NaCl' : 'b0b1C0C1',
-        'KCl'  : 'b0b1C0'  }
+# Load raw datasets
+datapath = 'datasets/'
+fpdbase,mols,ions = pz.data.fpd(datapath)
 
-aos = {'NaCl' : np.float_([-9, 2,-9,-9, 2.5]),
-        'KCl' : np.float_([-9, 2,-9,-9,-9  ])}
+# Select electrolytes for analysis
+fpdbase,mols,ions = pz.data.subset_ele(fpdbase,mols,ions,
+                                       np.array([Uele]))
 
-ionslist = {'NaCl' : np.array(['Na','Cl']),
-            'KCl'  : np.array(['K' ,'Cl'])}
+# Exclude smoothed datasets
+S = fpdbase.smooth == 0
+fpdbase = fpdbase[S]
+mols    = mols   [S]
 
-fc = fcs[ele]
-ao = aos[ele]
-ions = ionslist[ele]
-
-# Load FPD dataset
-with open('pickles/simpytz_fpd.pkl','rb') as f:
-    fpdbase,fpdp,err_cfs_both,fpd_sys_std = pickle.load(f)
-
-# Preprocess dataset
-L = fpdbase.ele == ele
-Eargs = [np.vstack(fpdbase.loc[L,var].values) \
-         for var in ['m', 'zC', 'zA', 'nC', 'nA', 'src', 'ele', 'fpd', 't25']]
-tot,zC,zA,nC,nA,srcs,eles,fpd,t25 = Eargs
-fpdbase = fpdbase[L]
-
-# Get pytzer fitting function inputs
-mCmA = np.concatenate([tot*nC,tot*nA], axis=1)
-T = np.full_like(tot,298.15, dtype='float64')
-alph1 = ao[1]
-alph2 = ao[2]
-omega = ao[4]
+# Create initial electrolytes pivot table
+fpde = pd.pivot_table(fpdbase,
+                      values  = ['m'  ],
+                      index   = ['ele'],
+                      aggfunc = [np.min,np.max,len])
 
 # Prepare model cdict
 cf = pz.cdicts.cdict()
+cf.add_zeros(fpdbase.ele)
+cf.dh['Aosm'] = pz.coeffs.Aosm_MPH
+cf.dh['AH'  ] = pz.coeffs.AH_MPH
 cf.bC['Na-Cl'] = pz.coeffs.bC_Na_Cl_A92ii
-cf.bC['K-Cl' ] = pz.coeffs.bC_K_Cl_A99
-cf.theta['K-Na'] = pz.coeffs.theta_zero
-cf.psi['K-Na-Cl'] = pz.coeffs.psi_zero
-cf.dh['Aosm']  = pz.coeffs.Aosm_M88
-cf.dh['AH']    = pz.coeffs.AH_MPH
 
-#%% Define optimisation function
-bs = np.vstack(fpdbase.ele.map(pz.prop.solubility25).values)
-T0 = pd2vs(fpdbase.t)
-T1 = pd2vs(fpdbase.t25)
-TR = pd2vs(fpdbase.t25)
-osm = pd2vs(fpdbase.osm25_calc)
+# Extract metadata from fpdbase
+tot  = pd2vs(fpdbase.m  )
+srcs = pd2vs(fpdbase.src)
+_,zC,zA,nC,nA = pz.data.znu(fpde.index)
 
+fpdbase['t25'] = 298.15
+T1    = pd2vs(fpdbase.t25)
+
+nCvec = pd2vs(fpdbase.nC)
+nAvec = pd2vs(fpdbase.nA)
+
+# Prepare for simulation
+for E,ele in enumerate(fpde.index):
+
+    EL = fpdbase.ele == ele
+    Eions = pz.data.ele2ions(np.array([ele]))[0]
+    
+    fpdbase['fpd_calc'] = pz.tconv.tot2fpd(tot[EL],Eions,nC[E],nA[E],cf)
+
+# Calculate osmotic coefficient etc.
+fpdbase['osm'] = pz.tconv.fpd2osm(mols,pd2vs(fpdbase.fpd))
+fpdbase['osm25'] = pz.tconv.osm2osm(tot,nCvec,nAvec,ions,
+                                    273.15-pd2vs(fpdbase.fpd),T1,T1,
+                                    cf,pd2vs(fpdbase.osm))
+fpdbase['osm25_calc'] = pz.model.osm(mols,ions,T1,cf)
+
+# Load outputs from simpytz_fpd.py
+with open('pickles/simpytz_fpd.pkl','rb') as f:
+    _,fpderr_rdm,fpderr_sys = pickle.load(f)
+    
+#%% Simulate new datasets
+
+# Set up for fitting
+alph1 = np.float_(2.5)
+alph2 = -9
+omega = np.float_(2)
+fpd_calc = pd2vs(fpdbase.fpd_calc)
+
+# Define weights for fitting
+#weights = np.ones(np.size(T1)) # uniform
+#weights = np.sqrt(tot) # sqrt of molality
+# ... based on random errors in each dataset:
+weights = np.full_like(tot,1, dtype='float64')
+for src in np.unique(srcs):
+    SL = srcs == src
+    weights[SL] = 1 / np.sqrt(np.sum(fpderr_rdm[ele][src]**2))
+weights = weights
+
+# Define fitting function
 def Eopt(rseed=None):
 
     # Seed random numbers
     np.random.seed(rseed)
 
-    Uosm = pz.sim.fpd(ele,tot,srcs,bs,osm,err_cfs_both,fpd_sys_std,
-                      fpd,nC,nA,ions,T0,T1,TR,cf)
+    # Simulate new FPD dataset
+    Ufpd = pz.sim.fpd(tot,pd2vs(fpdbase.fpd_calc),
+                      srcs,ele,fpderr_rdm,fpderr_sys)
+    
+    # Convert FPD to osmotic coefficient
+    Uosm = pz.tconv.fpd2osm(mols,Ufpd)
+    
+    # Convert osmotic coefficient to 298.15 K
+    Uosm25 = pz.tconv.osm2osm(tot,nCvec,nAvec,ions,
+                              273.15 - Ufpd,T1,T1,
+                              cf,Uosm)
 
-    b0,b1,b2,C0,C1,bCmx,mse \
-        = pz.fitting.bC(mCmA,zC,zA,T,alph1,alph2,omega,nC,nA,Uosm,fc,'osm')
+    # Solve for Pitzer model coefficients
+    b0,b1,b2,C0,C1,_,_ \
+        = pz.fitting.bC(mols,zC,zA,T1,alph1,alph2,omega,nC,nA,Uosm25,
+                        weights,'b0b1C0C1','osm')
 
     return b0,b1,b2,C0,C1
-
-#go = time()
-#Uosm = Eopt()
-#print(time()-go)
-#b0,b1,b2,C0,C1 = Eopt()
-
-Uosm = pz.sim.fpd(ele,tot,srcs,bs,osm,err_cfs_both,fpd_sys_std,
-                  fpd,nC,nA,ions,T0,T1,TR,cf)
 
 #%% Multiprocessing loop
 if __name__ == '__main__':
@@ -100,16 +127,20 @@ if __name__ == '__main__':
         pool.join()
 
     # Reformat pool output
-    bCpool = np.array([bCpool[X] for X in range(Ureps)])
-    bCpool_mean = np.mean(bCpool,axis=0)
-    bCpool_cv = np.cov(bCpool,rowvar=False)
-
-    Xtend = time() # end timer - multiprocessing
+    b0 = np.float_([bCpool[X][0] for X in range(Ureps)])
+    b1 = np.float_([bCpool[X][1] for X in range(Ureps)])
+    b2 = np.float_([bCpool[X][2] for X in range(Ureps)])
+    C0 = np.float_([bCpool[X][3] for X in range(Ureps)])
+    C1 = np.float_([bCpool[X][4] for X in range(Ureps)])
+    bC = np.vstack((b0,b1,b2,C0,C1))
+    bC_mean = np.mean(bC, axis=1)
+    bC_cv   = np.cov(bC)
 
     # Calculate and print processing time
+    Xtend = time() # end timer - multiprocessing
     print('multiprocessing %s: %d reps in %.2f seconds' \
-        % (ele,Ureps,(Xtend - Xtstart)))
+        % (Uele,Ureps,(Xtend - Xtstart)))
 
     # Pickle results
-    with open('pickles/simloop_pytzer_bC_' + ele + '.pkl','wb') as f:
-        pickle.dump((bCpool_mean,bCpool_cv,ele,Ureps),f)
+    with open('pickles/simloop_pytzer_bC_' + Uele + '.pkl','wb') as f:
+        pickle.dump((bC_mean,bC_cv,Uele,Ureps),f)
