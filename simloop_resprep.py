cd0e32d5d94b16258acd19cb50b2dab3a9c6cbd4
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-07-31 10:58:27 +0100

    Updating variable names for clarity and purging obsolete adjustments

--- simloop_resprep.py
+++ simloop_resprep.py
@@ -5,27 +5,17 @@ import pickle
 import pytzer as pz
 
 # Open simulation results
-with open('pickles/simloop_pytzer_bC_KCl_9600.pkl','rb') as f:
-    bC_mean,bC_cv,bCo,bCo_cv,Uele,Ureps = pickle.load(f)
+with open('pickles/simloop_pytzer_bC_NaCl_8.pkl','rb') as f:
+    bCsim,bCsim_cv,bCdir,bCdir_cv,Uele,Ureps = pickle.load(f)
 
-# Calculate activity coefficient
+#%% Calculate activity coefficient
 sqtot = np.vstack(np.linspace(0.001,1.81,100))
 tot   = sqtot**2
 mols  = np.concatenate((tot,tot),axis=1)
-#ions  = np.array(['K','Cl'])
 T     = np.full_like(tot,298.15)
 
-bCo_cv = np.insert(bCo_cv,(2,3),0, axis=1)
-bCo_cv = np.insert(bCo_cv,(2,3),0, axis=0)
-
-#cf = pz.cdicts.cdict()
-#cf.bC['K-Cl'] = pz.coeffs.bC_K_Cl_A99
-#cf.dh['Aosm'] = pz.coeffs.Aosm_MPH
-#
-#osm25 = pz.model.osm(mols,ions,T,cf)
-
 # Define propagation equation
-def ppg_acf(mCmA,zC,zA,T,bC,alph1,alph2,omega,nC,nA):
+def ppg_acfMX(mCmA,zC,zA,T,bC,alph1,alph2,omega,nC,nA):
     
     b0 = bC[0]
     b1 = bC[1]
@@ -36,7 +26,7 @@ def ppg_acf(mCmA,zC,zA,T,bC,alph1,alph2,omega,nC,nA):
     return pz.fitting.acfMX(mCmA,zC,zA,T,b0,b1,b2,C0,C1,
                             alph1,alph2,omega,nC,nA)
 
-fx_Jacf = jac(ppg_acf,argnum=4)
+fx_JacfMX = jac(ppg_acfMX,argnum=4)
 
 zC = np.float_(+1)
 zA = np.float_(-1)
@@ -46,16 +36,16 @@ alph1 = np.float_(2.5)
 alph2 = -9
 omega = -9
 
-acf   = ppg_acf(mols,zC,zA,T,bC_mean,alph1,alph2,omega,nC,nA)
-Jacf  = fx_Jacf(mols,zC,zA,T,bC_mean,alph1,alph2,omega,nC,nA).squeeze()
-Uacf  = np.vstack(np.diag(Jacf @ bC_cv @ Jacf.transpose()))
+acfMX_sim   = ppg_acfMX(mols,zC,zA,T,bCsim,alph1,alph2,omega,nC,nA)
+JacfMX_sim  = fx_JacfMX(mols,zC,zA,T,bCsim,alph1,alph2,omega,nC,nA).squeeze()
+UacfMX_sim  = np.vstack(np.diag(JacfMX_sim @ bCsim_cv @ JacfMX_sim.transpose()))
 
-acfo  = ppg_acf(mols,zC,zA,T,bCo,alph1,alph2,omega,nC,nA)
-Jacfo = fx_Jacf(mols,zC,zA,T,bCo,alph1,alph2,omega,nC,nA).squeeze()
-Uacfo = np.vstack(np.diag(Jacfo @ bCo_cv @ Jacfo.transpose()))
+acfMX_dir  = ppg_acfMX(mols,zC,zA,T,bCdir,alph1,alph2,omega,nC,nA)
+JacfMX_dir = fx_JacfMX(mols,zC,zA,T,bCdir,alph1,alph2,omega,nC,nA).squeeze()
+UacfMX_dir = np.vstack(np.diag(JacfMX_dir @ bCdir_cv @ JacfMX_dir.transpose()))
 
-savemat('pickles/simloop_res.mat',{'tot'  : tot,
-                                   'acf'  : np.vstack(acf),
-                                   'acfo' : np.vstack(acfo),
-                                   'Uacf' : Uacf,
-                                   'Uacfo': Uacfo})
+savemat('pickles/simloop_res.mat',{'tot'       : tot                 ,
+                                   'acfMX_sim' : np.vstack(acfMX_sim),
+                                   'acfMX_dir' : np.vstack(acfMX_dir),
+                                   'UacfMX_sim': UacfMX_sim          ,
+                                   'UacfMX_dir': UacfMX_dir          })
