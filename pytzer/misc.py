f1b30360438a985ca88048daf4bdb8c549300e1b
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-05 10:46:27 +0100

    Added Buck equation for pure water vapour pressure

--- pytzer/misc.py
+++ pytzer/misc.py
@@ -9,3 +9,11 @@ def pd2vs(series):
 # Inputs A and B should be numpy arrays
 def ismember(A,B):
     return xnp.logical_or.reduce([A == Bi for Bi in B])
+
+# Pure water vapour pressure following Buck (1981, 2012)
+#  see https://en.wikipedia.org/wiki/Arden_Buck_equation
+def vp_H2O(T):
+    # input  T  in K
+    # output VP in kPa
+    return 0.61121 * np.exp((18.678 \
+        - (T-273.15)/234.5) * ((T-273.15) / (257.14 + T - 273.15)))
