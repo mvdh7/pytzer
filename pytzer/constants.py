093b32ebe5cfc5ef7f73365f86dfdafbadec003f
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-05 10:46:11 +0100

    Added pressure unit conversion factors

--- pytzer/constants.py
+++ pytzer/constants.py
@@ -5,7 +5,10 @@ Mw = np.float_(0.018015)  # Molar mass of water  / kg/mol             [PubChem]
 R  = np.float_(8.3144598) # Universal gas const. / J /mol /K           [CODATA]
 F  = np.float_(96485.33289) # Faraday const.     / C /mol              [CODATA]
 b  = np.float_(1.2)       # Pitzer model coeff.  / sqrt(kg/mol)   [Pitzer 1991]
-
-cal2j = np.float_(4.184)  # Conversion factor    / cal/J
-
 Patm_bar = np.float_(1.01325) # Atmospheric pressure / bar
+
+# Unit conversion factors
+cal2j   = np.float_(4.184)         #  ENERGY  calorie    to Joule  / cal/J
+atm2pa  = np.float_(101325)        # PRESSURE atmosphere to Pascal / atm/Pa
+torr2pa = atm2pa / 760             # PRESSURE Torr       to Pascal / Torr/Pa
+mmHg2pa = np.float_(133.322387415) # PRESSURE mmHg       to Pascal / mmHg/Pa
