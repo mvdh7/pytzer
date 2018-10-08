ae5c1a294a8636ad1f77bd092b2ce7a5ccaf069e
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-08 14:44:57 +0100

    Updating capitalisation for unit conversions

--- pytzer/constants.py
+++ pytzer/constants.py
@@ -8,7 +8,7 @@ b  = np.float_(1.2)       # Pitzer model coeff.  / sqrt(kg/mol)   [Pitzer 1991]
 Patm_bar = np.float_(1.01325) # Atmospheric pressure / bar
 
 # Unit conversion factors
-cal2j   = np.float_(4.184)         #  ENERGY  calorie    to Joule  / cal/J
-atm2pa  = np.float_(101325)        # PRESSURE atmosphere to Pascal / atm/Pa
-torr2pa = atm2pa / 760             # PRESSURE Torr       to Pascal / Torr/Pa
-mmHg2pa = np.float_(133.322387415) # PRESSURE mmHg       to Pascal / mmHg/Pa
+cal2J   = np.float_(4.184)         #  ENERGY  calorie    to Joule  / cal/J
+atm2Pa  = np.float_(101325)        # PRESSURE atmosphere to Pascal / atm/Pa
+Torr2Pa = atm2Pa / 760             # PRESSURE Torr       to Pascal / Torr/Pa
+mmHg2Pa = np.float_(133.322387415) # PRESSURE mmHg       to Pascal / mmHg/Pa
