b77d96ac9bffed6adb66f225d67456596f77af2d
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-08 14:38:40 +0100

    Save output for MATLAB

--- vpl_prop.py
+++ vpl_prop.py
@@ -1,11 +1,12 @@
 from autograd import numpy as np
 from autograd import elementwise_grad as egrad
 import pytzer as pz
+from scipy.io import savemat
 
 # Define test conditions
-tot  = np.float_([[5]])
-T    = np.float_(298.15)
-vp0  = pz.misc.vp_H2O(T)
+tot  = np.vstack(np.linspace(0.01,6.25,500))
+T    = np.full_like(tot,298.15)
+vp0  = pz.misc.vp_H2O(T) # in kPa
 vpX  = vp0 * 0.5
 
 # Set up osmotic coefficient function and its derivatives
@@ -40,3 +41,8 @@ Uosm = np.sqrt(dosm_dtot**2 + dosm_dT**2 + dosm_dvpX**2)
 pct_dosm_dtot = 100 * np.abs(dosm_dtot) / Uosm
 pct_dosm_dT   = 100 * np.abs(dosm_dT  ) / Uosm
 pct_dosm_dvpX = 100 * np.abs(dosm_dvpX) / Uosm
+
+savemat('pickles/vpl_prop.mat',{'tot'      : tot,
+                                'dosm_dT'  : dosm_dT,
+                                'dosm_dtot': dosm_dtot,
+                                'dosm_dvpX': dosm_dvpX})
