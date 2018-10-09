1952fa76e338e8a4dbb75c25fae0633e6439473f
Matthew Humphreys <m.humphs@gmail.com>
2018-10-09 23:17:15 +0100

    Visualising results from data_iso.py

---
+++ data_iso.m
@@ -0,0 +1,21 @@
+isobase = readtable('pickles/data_iso.csv');
+srcs = unique(isobase.src);
+
+figure(1); clf; hold on
+
+for S = 1:numel(srcs)
+
+    SL = strcmp(isobase.src,srcs{S});
+    
+%     scatter(isobase.NaCl(SL), ...
+%         isobase.aw_ref_NaCl(SL) - isobase.aw_ref_KCl(SL))
+    
+    scatter(isobase.KCl(SL), ...
+        isobase.osm_meas_KCl(SL) - isobase.osm_ref_KCl(SL))
+    
+%     scatter(isobase.NaCl(SL), ...
+%         isobase.osm_meas_NaCl(SL) - isobase.osm_ref_NaCl(SL))
+    
+end %for S
+
+grid on
