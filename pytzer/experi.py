8d0998ecdc5836a76a10da96b20ee9750aed9bf1
Matthew Humphreys <m.humphs@gmail.com>
2018-10-10 12:38:37 +0100

    Added generic function to simulate perfect isopiestic equilibrium dataset

--- pytzer/experi.py
+++ pytzer/experi.py
@@ -1,5 +1,6 @@
 from autograd import numpy as np
 from autograd import elementwise_grad as egrad
+from scipy import optimize
 from . import data, fitting, model
 
 ##### ISOPIESTIC EQUILIBRIUM ##################################################
@@ -7,7 +8,7 @@ from . import data, fitting, model
 # Reference osmotic coefficient known
 def osm(mols,molsR,osmR):
     
-    return np.sum(molsR,axis=1) * osmR / np.sum(mols,axis=1)
+    return np.vstack(np.sum(molsR,axis=1) * osmR.ravel() / np.sum(mols,axis=1))
 
 # Reference osmotic coeff calculated from pz.model
 def osm_cf(mols,molsR,ionsR,T,cf):
@@ -30,4 +31,24 @@ def osm_bC(tot,totR,isopair,T,bCR):
     return osm(mols,molsR,osmR)
 
 # Derivatives
-dosm_dtot = egrad(osm_bC)
+dosm_dtot  = egrad(osm_bC)
+dosm_dtotR = egrad(osm_bC, argnum=1)
+
+# Simulate a perfect isopiestic dataset
+def get_osm(bC,T,molsR,osmR):
+    # bC is for the 'test' electrolyte to be simulated
+    # molsR and osmR are for the reference electrolyte
+    
+    def osm_tot(tot,zC,zA,T,bC):
+        mols = np.array([[tot,tot],[tot,tot]])
+        # ^cheap fix because pz.fitting.osm can't handle single-row mols
+        return fitting.osm(mols,zC,zA,T,*bC)
+
+    tot = np.full_like(osmR,np.nan)
+
+    for M in range(len(osmR)):
+        tot[M] = optimize.least_squares(lambda tot:
+            osm_tot(tot,1.,-1.,np.vstack(T[M]),bC)[0] * (tot + tot) \
+                           - osmR[M] * np.sum(molsR[M]), 1.)['x']
+            
+    return tot
