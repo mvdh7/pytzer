e2fb0e0cf284ce56a0e944361e9184ebac842167
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-15 14:00:35 +0100

    Misc. replotting

--- simpar_fpd.m
+++ simpar_fpd.m
@@ -20,7 +20,7 @@ for S = 1:numel(fpdsrcs.all.srcs)
 end %for S
 mksz = 10;
 
-for E = 1%:numel(eles)
+for E = 1:numel(eles)
 ele = eles{E};
 
 % Define settings that depend upon electrolyte
@@ -46,7 +46,7 @@ EL = strcmp(fpdbase.ele,ele);
 fpdsrcs.(ele).srcs = unique(fpdbase.src(EL));
 
 % Begin figure
-figure(2); clf
+figure(E); clf
 printsetup(gcf,[9 12])
 flegs = {};
 
