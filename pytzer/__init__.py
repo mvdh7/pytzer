283874ba1b4648f804d0cc3072fe1994e20444ca
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-15 14:46:32 +0100

    Add new isoref module

--- pytzer/__init__.py
+++ pytzer/__init__.py
@@ -1,7 +1,7 @@
 name = 'pytzer'
 
 __all__ = ['cdicts', 'coeffs', 'constants', 'data', 'experi', 'fitting', 'io',
-           'jfuncs', 'misc', 'model', 'prop', 'sim', 'tconv']
+           'isoref', 'jfuncs', 'misc', 'model', 'prop', 'sim', 'tconv']
 
-from . import cdicts, coeffs, constants, data, experi, fitting, io, jfuncs, \
-    misc, model, prop, sim, tconv
+from . import cdicts, coeffs, constants, data, experi, fitting, io, isoref, \
+              jfuncs, misc, model, prop, sim, tconv
