fa7e4d497aa0d7c0b8295c293803d65437a90ad9
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-23 09:45:49 +0100

    get_isotot is faulty - use alternative in simloop_iso

--- pytzer/experi.py
+++ pytzer/experi.py
@@ -39,24 +39,29 @@ def osm_bC(tot,totR,isopair,T,bCR):
 dosm_dtot  = egrad(osm_bC)
 dosm_dtotR = egrad(osm_bC, argnum=1)
 
-# Simulate a perfect isopiestic dataset
-def get_osm(bC,T,molsR,osmR):
-    # bC is for the 'test' electrolyte to be simulated
+# Simulate a perfect isopiestic dataset - FAULTY!!! SEE ALTERNATIVE IN SIMLOOP
+def get_isotot(bCT,nuT,zCT,zAT,nCT,nAT,T,molsR,osmR):
+    # bC & nu are for the 'test' electrolyte to be simulated
     # molsR and osmR are for the reference electrolyte
     
-    def osm_tot(tot,zC,zA,T,bC):
-        mols = np.array([[tot,tot],[tot,tot]])
+    def osm_tot(tot,zC,zA,nC,nA,T,bC,M):
+        mols = np.array([[tot*nC,tot*nA],[tot*nC,tot*nA]])
         # ^cheap fix because pz.fitting.osm can't handle single-row mols
-        return fitting.osm(mols,zC,zA,T,*bC)
+        return fitting.osm(mols,zC,zA,T,*[bC[X][M] for X in range(5)],
+                           *[bC[X] for X in range(5,8)])
 
-    tot = np.full_like(osmR,np.nan)
+    totT = np.full_like(osmR,np.nan)
 
     for M in range(len(osmR)):
-        tot[M] = optimize.least_squares(lambda tot:
-            osm_tot(tot,1.,-1.,np.vstack(T[M]),bC)[0] * (tot + tot) \
-                           - osmR[M] * np.sum(molsR[M]), 1.)['x']
+        totT[M] = optimize.least_squares(lambda totT:
+            (osm_tot(totT,zCT[0],zAT[0],nCT[0],nAT[0],
+                     np.vstack(T[M]),bCT,M)[0] * nuT * totT \
+            - osmR[M] * np.sum(molsR[M])).ravel(), 1.)['x']
             
-    return tot
+    osmT = fitting.osm(np.concatenate((totT*nCT,totT*nAT),axis=1),
+                       zCT,zAT,T,*bCT)
+            
+    return totT, osmT
 
 # Define functions to fit isopiestic residuals
 def isofit_sys(isoerr,tot):
