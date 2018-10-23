50ba855308fdefd9c5167c89f9766e10632d8a9e
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-23 10:21:16 +0100

    Multiprocessing loop all working except that ref model is fixed

--- simloop_iso.py
+++ simloop_iso.py
@@ -1,9 +1,14 @@
 from autograd import numpy as np
+from multiprocessing import Pool
 import pandas as pd
 import pickle
 import pytzer as pz
 from pytzer.misc import pd2vs
 from scipy import optimize
+from scipy.io import savemat
+from time import time
+
+Ureps = int(10)
 
 # Define test and reference electrolytes
 tst = 'KCl'
@@ -87,3 +92,79 @@ def Eopt(rseed=None):
 
     return b0,b1,b2,C0,C1
 
+#%% Multiprocessing loop
+if __name__ == '__main__':
+
+    print('multiprocessing %s...' % tst, end='\r')
+
+    # Set initial random seed (for reproducibility)
+    np.random.seed(295)
+
+    # Generate seeds for random number generator
+    rseeds = np.random.randint(0,2**32,size=Ureps,dtype='int64')
+
+    Xtstart = time() # begin timer - multiprocessing
+
+    with Pool() as pool:
+        bCpool = pool.map(Eopt,rseeds)
+        pool.close()
+        pool.join()
+
+    # Reformat pool output
+    b0 = np.float_([bCpool[X][0] for X in range(Ureps)])
+    b1 = np.float_([bCpool[X][1] for X in range(Ureps)])
+    b2 = np.float_([bCpool[X][2] for X in range(Ureps)])
+    C0 = np.float_([bCpool[X][3] for X in range(Ureps)])
+    C1 = np.float_([bCpool[X][4] for X in range(Ureps)])
+    bC = np.vstack((b0,b1,b2,C0,C1))
+    bCsim = np.mean(bC, axis=1)
+    bCsim_cv   = np.cov(bC)
+
+    # Calculate and print processing time
+    Xtend = time() # end timer - multiprocessing
+    print('multiprocessing %s: %d reps in %.1f seconds' \
+        % (tst,Ureps,(Xtend - Xtstart)))
+
+    # Calculate activity coefficient and propagate error with sim. results
+    sqtot = np.vstack(np.linspace(0.001,
+                                  np.sqrt(pz.prop.solubility25[tst]),
+                                  100))
+    tot   = sqtot**2
+    mols  = np.concatenate((tot,tot),axis=1)
+    T     = np.full_like(tot,298.15)
+
+    # Get example propagation splines
+    acfMX_sim, UacfMX_sim = pz.fitting.ppg_acfMX(mols,tzC,tzA,T,bCsim,bCsim_cv,
+                                                 talph1,talph2,tomega,tnC,tnA)
+
+    acfMX_dir, UacfMX_dir = pz.fitting.ppg_acfMX(mols,tzC,tzA,T,bCdir,bCdir_cv,
+                                                 talph1,talph2,tomega,tnC,tnA)
+
+    osm_sim, Uosm_sim = pz.fitting.ppg_osm(mols,tzC,tzA,T,bCsim,bCsim_cv,
+                                           talph1,talph2,tomega)
+
+    osm_dir, Uosm_dir = pz.fitting.ppg_osm(mols,tzC,tzA,T,bCdir,bCdir_cv,
+                                           talph1,talph2,tomega)
+
+    # Pickle/save results...
+    fstem = 'pickles/simloop_iso_bC_' + trtxt + '_' + str(Ureps)
+    # ...for Python:
+    with open(fstem + '.pkl','wb') as f:
+        pickle.dump((bCsim,bCsim_cv,bCdir,bCdir_cv,tst,ref,Ureps),f)
+    # ...for MATLAB:
+    savemat(fstem + '.mat', {'bCsim'     : bCsim     ,
+                             'bCsim_cv'  : bCsim_cv  ,
+                             'bCdir'     : bCdir     ,
+                             'bCdir_cv'  : bCdir_cv  ,
+                             'tst'       : tst       ,
+                             'ref'       : ref       ,
+                             'Ureps'     : Ureps     ,
+                             'tot'       : tot       ,
+                             'acfMX_sim' : acfMX_sim ,
+                             'acfMX_dir' : acfMX_dir ,
+                             'UacfMX_sim': UacfMX_sim,
+                             'UacfMX_dir': UacfMX_dir,
+                             'osm_sim'   : osm_sim   ,
+                             'Uosm_sim'  : Uosm_sim  ,
+                             'osm_dir'   : osm_dir   ,
+                             'Uosm_dir'  : Uosm_dir  })
