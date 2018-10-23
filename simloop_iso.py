265f0163d53bf903c1e29786eda1074d611621fd
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-23 09:46:32 +0100

    Solve for test molality at perfect equilibrium

--- simloop_iso.py
+++ simloop_iso.py
@@ -3,6 +3,7 @@ import pandas as pd
 import pickle
 import pytzer as pz
 from pytzer.misc import pd2vs
+from scipy import optimize
 
 # Define test and reference electrolytes
 tst = 'KCl'
@@ -24,12 +25,35 @@ rtot        = pd2vs(isobase[ref])
 rosm25_calc = pd2vs(isobase['osm_calc_' + ref])
 srcs        = pd2vs(isobase.src)
 T           = pd2vs(isobase.t)
-_,_,_,rnC,rnA = pz.data.znu([ref])
 
-tbC = cf.bC['K-Cl'](T)
-rmols = np.concatenate((rtot*rnC,rtot*rnA),axis=1)
+# Get electrolyte info
+_,_,_,tnC,tnA = pz.data.znu([tst])
+tions = pz.data.ele2ions([tst])[0]
 
-tosm25_calc = pz.experi.get_osm(tbC,T,rmols,rosm25_calc)
+#%% Solve for expected test molality & osmotic coefficient
+def tot2osm(tot,nC,nA,ions,T,cf):
+    mols = np.concatenate((tot*nC,tot*nA),axis=1)
+    return pz.model.osm(mols,ions,T,cf)
+
+ttot_calc = np.full_like(rtot,np.nan)
+
+for M in range(len(ttot_calc)):
+    
+    ttot_calc[M] = optimize.least_squares(lambda ttot: \
+        rtot[M]*rosm25_calc[M] - ttot*tot2osm(np.vstack([ttot]),tnC,tnA,
+            tions,np.vstack([T[M]]),cf).ravel(), 1.)['x']
+    
+tmols = np.concatenate((ttot_calc*tnC,ttot_calc*tnA),axis=1)
+tosm25_calc = pz.model.osm(tmols,tions,T,cf)
+
+#%%
+#from scipy import optimize
+
+#test = pz.fitting.osm(rmols,rzC,rzA,T,rbC)
+
+#test = optimize.least_squares(lambda tot:
+#    pz.fitting.osm(mols,zC,zA,T,*bC) * nu * tot \
+#    - osmR[M] * np.sum(molsR[M]), 1.)['x']
 
 ## Test simulation function
 #isobase['osm25_sim'] = pz.sim.iso(rtot,tosm25_calc,srcs,tst,ref,
