3276af5b868a4521df5a278e84ba7298fb82e33a
Matthew Humphreys <mvdh7@users.noreply.github.com>
2018-10-24 12:16:37 +0100

    Investigating Sn estimator for SD

--- simpar_iso_all.m
+++ simpar_iso_all.m
@@ -7,9 +7,20 @@ bw = 1;
 histogram(isoerr_sys.all,(-8:bw:8)*1e-3, 'normalization','count', ...
     'facecolor',0.3*[1 1 1])
 
+MAD = median(abs(isoerr_sys.all - median(isoerr_sys.all)));
+sd_MAD = MAD * 1.4826;
+
+% Sn0 = NaN(numel(isoerr_sys.all));
+% Sn1 = NaN(numel(isoerr_sys.all),1);
+% for C = 1:numel(isoerr_sys.all)
+%     Sn0(:,C) = abs(isoerr_sys.all(C) - isoerr_sys.all);
+%     Sn1(C) = median(Sn0(Sn0(:,C) ~= 0,C));
+% end %for C
+% sd_Sn = 1.1926 * median(Sn1);
+
 mu = 0;
 fx = (-9:0.01:9)*1e-3;
-fy = normpdf(fx,mu,isoerr_sys.all_qsd) ...
+fy = normpdf(fx,mu,isoerr_sys.all_Sn) ...isoerr_sys.all_qsd) ...
     * bw*1e-3 * numel(isoerr_sys.all);
 
 % ld = isoerr_sys.all_laplace;
