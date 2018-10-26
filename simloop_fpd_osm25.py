Fixing mols calculation for spline generation

simloop_fpd_osm25.py
@@ -155,9 +155,10 @@ if __name__ == '__main__':
                                   np.sqrt(pz.prop.solubility25[Uele]),
                                   100))
     tot   = sqtot**2
-    mols  = np.concatenate((tot,tot),axis=1)
-    T     = np.full_like(tot,298.15)
+    
     _,zC,zA,nC,nA = pz.data.znu([Uele])
+    mols  = np.concatenate((tot*nC,tot*nA),axis=1)
+    T     = np.full_like(tot,298.15)
     
     # Get example propagation splines
     acfMX_sim, UacfMX_sim = pz.fitting.ppg_acfMX(mols,zC,zA,T,bCsim,bCsim_cv,