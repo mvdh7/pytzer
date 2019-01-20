1e48af6a898a1da1fa43d9bb442c7e0948b1b38d
Matthew Humphreys <m.humphs@gmail.com>
2019-01-20 12:02:40 +0000

    Moved to pytzer-support repo

--- archive/omo/mwe_omo.py
+++
@@ -1,9 +0,0 @@
-from pyomo.environ import *
-
-model = ConcreteModel()
-
-model.x = Var( initialize=-1.2, bounds=(-50, 50) )
-
-model.obj = Objective(
-    expr= model.x - 5.,
-    sense= minimize )
--- archive/xpHDG.m
+++
@@ -1,6 +0,0 @@
-load('xpHDG.mat');
-
-figure(1); clf; hold on
-
-plot(pH,DG.^2);
-ylim([0 1000])
