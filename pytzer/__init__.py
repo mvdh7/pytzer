dfeaf0e3a4d9411c08ed8b93520fccdd31e8050b
Matthew Humphreys <m.humphs@gmail.com>
2019-01-20 12:16:36 +0000

    Cut down to modules still included

--- pytzer/__init__.py
+++ pytzer/__init__.py
@@ -1,7 +1,5 @@
 name = 'pytzer'
 
-__all__ = ['cdicts', 'coeffs', 'constants', 'data', 'experi', 'fitting', 'io',
-           'isoref', 'jfuncs', 'misc', 'model', 'prop', 'sim', 'tconv']
+__all__ = ['cdicts', 'coeffs', 'constants', 'io', 'jfuncs', 'misc', 'model']
 
-from . import cdicts, coeffs, constants, data, experi, fitting, io, isoref, \
-              jfuncs, misc, model, prop, sim, tconv
+from . import cdicts, coeffs, constants, io, jfuncs, misc, model
