799aedeb01e0f045ec7be81681fb740085bb0d69
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-21 12:24:14 +0000

    Add new props module

--- pytzer/__init__.py
+++ pytzer/__init__.py
@@ -1,5 +1,6 @@
 name = 'pytzer'
 
-__all__ = ['cdicts', 'coeffs', 'constants', 'io', 'jfuncs', 'misc', 'model']
+__all__ = ['cdicts', 'coeffs', 'constants', 'io', 'jfuncs', 'misc', 'model',
+           'props']
 
-from . import cdicts, coeffs, constants, io, jfuncs, misc, model
+from . import cdicts, coeffs, constants, io, jfuncs, misc, model, props
