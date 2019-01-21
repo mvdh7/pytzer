52daeb4ed51bcf2ba050f570a9bb6f6079e27681
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-21 16:32:35 +0000

    Continue testing simplified pytzer

--- pztest.py
+++ pztest.py
@@ -1,12 +1,26 @@
 import pytzer as pz
+import numpy as np
+from autograd import elementwise_grad as egrad
 
 filename = 'testfiles/M88 Table 4.csv'
-delim = ','
+#delim = ','
 
-mols, ions, T = pz.io.getmols(filename,delim)
+mols,ions,T = pz.io.getmols(filename)
 
-cf = pz.cdicts.M88
+cf = pz.cfdicts.M88
 
 osm = pz.model.osm(mols,ions,T,cf)
 
-aw = pz.model.osm2aw(mols, osm)
+aw = pz.model.osm2aw(mols,osm)
+
+acfs = pz.model.acfs(mols,ions,T,cf)
+
+tx = np.array([1])
+
+tJ = pz.jfuncs._Harvie_J(tx)
+
+tJscipy = pz.jfuncs._Harvie_J_drv(tx)
+
+fJegrad = egrad(pz.jfuncs._Harvie_J)
+
+tJegrad = fJegrad(tx)
