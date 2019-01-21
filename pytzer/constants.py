2cfea20d4162a0131686e7f92e7e62046f736ee3
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-21 16:30:28 +0000

    Tidy up comments

--- pytzer/constants.py
+++ pytzer/constants.py
@@ -1,11 +1,11 @@
 from autograd.numpy import float_
 
 # Set constant values
-Mw = float_(0.018015)     # Molar mass of water  / kg/mol             [PubChem]
-R  = float_(8.3144598)    # Universal gas const. / J /mol /K           [CODATA]
-F  = float_(96485.33289)    # Faraday const.     / C /mol              [CODATA]
-b  = float_(1.2)          # Pitzer model coeff.  / sqrt(kg/mol)   [Pitzer 1991]
-Patm_bar = float_(1.01325) # Atmosphere pressure / bar
+Mw = float_(0.018015)      # Molar mass of water  / kg/mol            [PubChem]
+R  = float_(8.3144598)     # Universal gas const. / J /mol /K          [CODATA]
+F  = float_(96485.33289)   #   Faraday const.     / C /mol             [CODATA]
+b  = float_(1.2)           # Pitzer model coeff.  / sqrt(kg/mol)  [Pitzer 1991]
+Patm_bar = float_(1.01325) # Atmospheric pressure / bar
 
 # Unit conversion factors
 cal2J   = float_(4.184)         #  ENERGY  calorie    to Joule  / cal/J
