b19c0b0dda51303816322b54104431d8ecf9c824
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-21 16:29:25 +0000

    ele2ions not needed in this implementation

--- pytzer/io.py
+++ pytzer/io.py
@@ -1,7 +1,7 @@
-from autograd.numpy import array, concatenate, genfromtxt, nan_to_num, \
-                           shape, unique
+from autograd.numpy import genfromtxt, nan_to_num, shape
 
-##### FILE I/O ################################################################
+#==============================================================================
+#=================================================== Import molality data =====
 
 def getmols(filename, delimiter=','):
 
@@ -18,25 +18,3 @@ def getmols(filename, delimiter=','):
     T = data[:,TL]
     
     return mols, ions, T
-
-# Return list of unique ions from list of electrolytes
-def ele2ions(ele):
-
-    # Define ions in each electrolyte given full dissociation
-    idict = {'BaCl2'  : array(['Ba', 'Cl' ]),
-             'CaCl2'  : array(['Ca', 'Cl' ]),
-             'H2SO4'  : array(['H' , 'SO4']),
-             'KCl'    : array(['K' , 'Cl' ]),
-             'KOH'    : array(['K' , 'OH' ]),
-             'MgCl2'  : array(['Mg', 'Cl' ]),
-             'Na2SO4' : array(['Na', 'SO4']),
-             'NaCl'   : array(['Na', 'Cl' ]),
-             'ZnBr2'  : array(['Zn', 'Br' ]),
-             'ZnCl2'  : array(['Zn', 'Cl' ])}
-
-    cats = unique(array([idict[ionpair][0] for ionpair in ele]).ravel())
-    anis = unique(array([idict[ionpair][1] for ionpair in ele]).ravel())
-
-    ions = concatenate((cats,anis))
-
-    return ions, cats, anis, idict
