1acd6e005479dfa71f68e4031274f7a3be11d79c
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-22 10:31:48 +0000

    Correct units

--- pytzer/constants.py
+++ pytzer/constants.py
@@ -5,10 +5,10 @@ Mw = float_(0.018015)      # Molar mass of water  / kg/mol            [PubChem]
 R  = float_(8.3144598)     # Universal gas const. / J /mol /K          [CODATA]
 F  = float_(96485.33289)   #   Faraday const.     / C /mol             [CODATA]
 b  = float_(1.2)           # Pitzer model coeff.  / sqrt(kg/mol)  [Pitzer 1991]
-Patm_bar = float_(1.01325) # Atmospheric pressure / bar
+Patm_bar = float_(1.01325) # Atmospheric pressure / bar/atm
 
 # Unit conversion factors
 cal2J   = float_(4.184)         #  ENERGY  calorie    to Joule  / cal/J
-atm2Pa  = float_(101325)        # PRESSURE atmosphere to Pascal / atm/Pa
-Torr2Pa = atm2Pa / 760          # PRESSURE Torr       to Pascal / Torr/Pa
-mmHg2Pa = float_(133.322387415) # PRESSURE mmHg       to Pascal / mmHg/Pa
+atm2Pa  = float_(101325)        # PRESSURE atmosphere to Pascal / Pa/atm
+Torr2Pa = atm2Pa / 760          # PRESSURE Torr       to Pascal / Pa/Torr
+mmHg2Pa = float_(133.322387415) # PRESSURE mmHg       to Pascal / Pa/mmHg
