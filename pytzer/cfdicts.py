a82bb13ffc964f36a1c18569dbd121951efd2093
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-01-23 16:04:26 +0000

    Printed coefficients for MarChemSpec model to file

--- pytzer/cfdicts.py
+++ pytzer/cfdicts.py
@@ -95,6 +95,85 @@ class CoefficientDictionary:
             innn = '-'.join((neutral,neutral,neutral))
             if innn not in self.mu.keys():
                 self.mu[innn] = coeffs.mu_zero
+                
+    # Print all coefficient values at a given temperature
+    def print_coeffs(self,T,filename):
+        
+        f = open(filename,'w')
+        
+        f.write('c-a: b0, b1, b2, C0, C1, alph1, alph2, omega\n')
+        f.write('============================================\n')
+        
+        for bC in self.bC.keys():
+            
+            cation,anion = bC.split('-')
+            b0,b1,b2,C0,C1, alph1,alph2,omega, _ = self.bC[bC](T)
+            
+            f.write(''.join(('{:6s} {:6s} {:>12.5e} {:>12.5e} {:>12.5e} ',
+                             '{:>12.5e} {:>12.5e} {:>5.2f} {:>5.2f} {:>5.2f}\n' \
+                             )).format(cation,anion,b0,b1,b2,C0,C1,
+                                       alph1,alph2,omega))
+    
+        f.write('\n')
+        f.write('i-i\': theta\n')
+        f.write('===========\n')
+    
+        for theta in self.theta.keys():
+            
+            ion0,ion1 = theta.split('-')
+            eval_theta = self.theta[theta](T)[0]
+            
+            f.write('{:6s} {:6s} {:>12.5e}\n'.format(ion0,ion1,eval_theta))
+            
+        f.write('\n')
+        f.write('i-i\'-j: psi\n')
+        f.write('===========\n')
+            
+        for psi in self.psi.keys():
+            
+            ion0,ion1,ion2 = psi.split('-')
+            eval_psi = self.psi[psi](T)[0]
+            
+            f.write('{:6s} {:6s} {:6s} {:>12.5e}\n'.format(ion0,ion1,ion2,
+                                                           eval_psi))
+
+        f.write('\n')
+        f.write('n-i: lambd\n')
+        f.write('==========\n')
+            
+        for lambd in self.lambd.keys():
+            
+            neutral0,neutral1 = lambd.split('-')
+            eval_lambd = self.lambd[lambd](T)[0]
+            
+            f.write('{:6s} {:6s} {:>12.5e}\n'.format(neutral0,neutral1,
+                                                     eval_lambd))
+            
+        f.write('\n')
+        f.write('n-c-a: eta\n')
+        f.write('==========\n')
+            
+        for eta in self.eta.keys():
+            
+            neutral,cation,anion = eta.split('-')
+            eval_eta = self.eta[eta](T)[0]
+            
+            f.write('{:6s} {:6s} {:6s} {:>12.5e}\n'.format(neutral,cation,
+                                                           anion,eval_eta))
+            
+        f.write('\n')
+        f.write('n-n-n: mu\n')
+        f.write('=========\n')
+            
+        for mu in self.mu.keys():
+            
+            neutral = mu.split('-')[0]
+            eval_mu = self.mu[mu](T)[0]
+            
+            f.write('{:6s} {:6s} {:6s} {:>12.5e}\n'.format(neutral,neutral,
+                                                           neutral,eval_mu))
+            
+        f.close()
             
 
 #==============================================================================
--- pztest.py
+++ pztest.py
@@ -1,5 +1,5 @@
 import pytzer as pz
-import numpy as np
+#import numpy as np
 
 filename = 'testfiles/GenerateConcs.csv'
 
@@ -7,19 +7,21 @@ mols,ions,T = pz.io.getmols(filename)
 
 cf = pz.cfdicts.MarChemSpec
 
-# Cut out zero ionic strengths
+cf.print_coeffs(298.15,'print_coeffs/MarChemSpec25.txt')
 
-zs = pz.props.charges(ions)[0]
-I = np.vstack(0.5 * (np.sum(mols * zs**2, 1)))
-
-Gex_nRT = np.full_like(T   , np.nan)
-osm     = np.full_like(T   , np.nan)
-acfs    = np.full_like(mols, np.nan)
-
-L = (I > 0).ravel()
-
-nargs = (mols[L,:], ions, T[L], cf)
-
-Gex_nRT[L  ] = pz.model.Gex_nRT(*nargs)
-osm    [L  ] = pz.model.osm    (*nargs)
-acfs   [L,:] = pz.model.acfs   (*nargs)
+## Cut out zero ionic strengths
+#
+#zs = pz.props.charges(ions)[0]
+#I = np.vstack(0.5 * (np.sum(mols * zs**2, 1)))
+#
+#Gex_nRT = np.full_like(T   , np.nan)
+#osm     = np.full_like(T   , np.nan)
+#acfs    = np.full_like(mols, np.nan)
+#
+#L = (I > 0).ravel()
+#
+#nargs = (mols[L,:], ions, T[L], cf)
+#
+#Gex_nRT[L  ] = pz.model.Gex_nRT(*nargs)
+#osm    [L  ] = pz.model.osm    (*nargs)
+#acfs   [L,:] = pz.model.acfs   (*nargs)
