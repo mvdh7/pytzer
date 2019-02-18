dc99f22038322de5e2cbb6fb6b0b384d3391228c
Matthew <m.humphs@gmail.com>
2019-02-18 23:04:20 +0000

    Compare pytzer vs Julia, and replicate MATLAB figure

--- pztest.py
+++ pztest.py
@@ -1,77 +1,39 @@
 #from copy import deepcopy
 import pytzer as pz
-#import numpy as np 
+import numpy as np 
 
 # Import test dataset
-filename = 'testfiles/pytzerPitzer.csv'
+filepath = 'testfiles/'
+filestem = 'pytzerPitzer'
 
-pz.blackbox(filename)
+# Run black box calculation (uses MarChemSpec cfdict)
+mols,ions,T,cf,Gex_nRT,osm,aw,acfs = pz.blackbox(filepath + filestem + '.csv')
 
-#mols,ions,T = pz.io.getmols(filename)
+# Load results from Julia
+jfilename = filepath + filestem + '_jl.csv'
+jdata = np.genfromtxt(jfilename, delimiter=',', skip_header=1)
+jhead = np.genfromtxt(jfilename, delimiter=',', skip_footer=np.shape(jdata)[0],
+                      dtype='str')
+                  
+gions = [''.join(('g',ion)) for ion in ions]    
+jacfs = np.concatenate([np.vstack(jdata[:,C]) 
+    for C in range(np.shape(jdata)[1])
+    if jhead[C] in gions],
+    axis=1)
 
-### Change temperature
-##TEMP = np.float_(273.15)
-##T[:] = TEMP # just in case
-#
-## Set up CoefficientDictionary
-#cf = deepcopy(pz.cfdicts.MarChemSpec)
-#cf.add_zeros(ions) # just in case
-#
-### Print out coefficients at 298.15 K
-##cf.print_coeffs(TEMP,'print_coeffs/' + cf.name + '.txt')
-#
-## Separate out zero ionic strengths
-#zs = pz.props.charges(ions)[0]
-#I = np.vstack(0.5 * (np.sum(mols * zs**2, 1)))
-#
-#Gex_nRT = np.full_like(T   , np.nan)
-#osm     = np.full_like(T   , np.nan)
-#acfs    = np.full_like(mols, np.nan)
-#
-#L = (I > 0).ravel()
-#
-#nargsL  = (mols[ L,:], ions, T[ L], cf)
-#nargsLx = (mols[~L,:], ions, T[~L], cf)
-#
-## Do calculations
-#print('Calculating excess Gibbs energies...')
-#Gex_nRT[ L] = pz.model.Gex_nRT(*nargsL)
-#if np.any(~L):
-#    Gex_nRT[~L] = pz.model.Gex_nRT(*nargsLx, Izero=True)
-#
-#print('Calculating osmotic coefficients...')
-#osm[ L] = pz.model.osm(*nargsL)
-#if np.any(~L):
-#    osm[~L] = pz.model.osm(*nargsLx, Izero=True)
-#
-#print('Calculating water activity...')
-#aw = pz.model.osm2aw(mols,osm)
-#
-#print('Calculating activity coefficients...')
-#acfs[ L,:] = pz.model.acfs(*nargsL)
-#if np.any(~L):
-#    acfs[~L,:] = pz.model.acfs(*nargsLx, Izero=True)
-#
-#### Loop it
-###for i in range(len(T)):
-###    
-###    mols_i = np.array([mols[i,:]])
-###    T_i    = np.array([T[i]])
-###    
-###    nargs_i = (mols_i, ions, T_i, cf)
-###    
-###    Gex_nRT[i]   = pz.model.Gex_nRT(*nargs_i)
-###    osm    [i]   = pz.model.osm    (*nargs_i)
-###    acfs   [i,:] = pz.model.acfs   (*nargs_i)
-##    
-### Save results for plotting in MATLAB
-##from scipy.io import savemat
-##savemat('testfiles/threeway/temptestUpVec.mat',
-##        {'mols'   : mols   ,
-##         'ions'   : ions   ,
-##         'T'      : T      ,
-###         'Gex_nRT': Gex_nRT,
-##         'acfs'   : acfs   ,
-##         'osm'    : osm    ,
-###         'aw'     : aw     ,
-##         'I'      : I      })
+# Compare and plot differences
+dacfs = jacfs - acfs
+
+from matplotlib import pyplot as plt
+
+fig,ax = plt.subplots()
+
+vminmax = np.max(np.abs(dacfs))
+cax = ax.imshow(dacfs, cmap="coolwarm", aspect='auto',
+                vmin=-vminmax, vmax=vminmax)
+fig.colorbar(cax, label='Diff. in acf')
+
+plt.xticks(range(len(ions)), rotation=0)
+ax.set_xticklabels(ions)
+
+ax.set_ylabel('Row in file')
