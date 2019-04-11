3ca1536fa78a3fe5a9d1a0122cf1a054dcd05e54
Matthew <m.humphs@gmail.com>
2019-04-11 23:07:14 +0100

    More testing

--- setup.py
+++ setup.py
@@ -20,6 +20,7 @@ setuptools.setup(
     long_description_content_type = 'text/markdown',
     classifiers = (
         'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
