2cd35835d69c280278182d28ae727c1668a6b5b0
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-04-16 11:03:37 +0100

    Add D-H component calculation

--- mxmodel.py
+++ mxmodel.py
@@ -1,9 +1,24 @@
 import pytzer as pz
 from autograd import numpy as np
 
-mols = np.array([1.5, 1.5])
-zs = np.array([1.0, -1.0])
+# Define inputs
+mols_mx = np.array([1.5, 1.5])
+ions = np.array(['Na', 'Cl'])
+tempK = np.array([298.15])
+pres = np.array([10.1325])
 
-Istr_pz = pz.model.Istr(mols, zs)
-Istr_mx = pz.matrix.Istr(mols, zs)
+# Calculate intermediates
+mols_pz = np.vstack(mols_mx)
+zs_pz = pz.props.charges(ions)[0]
+zs_mx = zs_pz.ravel()
+Aosm = pz.debyehueckel.Aosm_AW90(tempK, pres)[0][0]
+
+# Calculate ionic strength
+Istr_pz = pz.model.Istr(mols_pz, zs_pz)
+Istr_mx = pz.matrix.Istr(mols_mx, zs_mx)
+
+# Calculate Debye-Hueckel term
+fG_pz = pz.model.fG(tempK, pres, Istr_pz, pz.cflibs.Seawater)
+fG_mx = pz.matrix.fG(Aosm, Istr_mx)
+
 
--- pytzer/matrix.py
+++ pytzer/matrix.py
@@ -1,10 +1,18 @@
 # Pytzer: Pitzer model for chemical activities in aqueous solutions
 # Copyright (C) 2019  Matthew Paul Humphreys  (GNU GPLv3)
 
-from autograd.numpy import transpose
+from autograd.numpy import log, sqrt, transpose
+from .constants import b
+from .model import g
 
 """Matrix version of the Pitzer model."""
 
 def Istr(mols, zs):
-    """Calculate ionic strength."""
+    """Calculate the ionic strength."""
     return mols @ transpose(zs**2) / 2
+
+def fG(Aosm, I):
+    """Calculate the Debye-Hueckel component of the excess Gibbs energy."""
+    return -4*I*Aosm * log(1 + b*sqrt(I)) / b
+
+def B
