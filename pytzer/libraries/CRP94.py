72f528bd83472bd89965a714c2039d051165744e
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-07-19 16:44:00 +0100

    Fix first few parameter libraries to new format

--- pytzer/libraries/CRP94.py
+++ pytzer/libraries/CRP94.py
@@ -2,16 +2,14 @@
 # Copyright (C) 2019  Matthew Paul Humphreys  (GNU GPLv3)
 from .. import parameters as prm
 from .. import debyehueckel, unsymmetrical
-from . import ParameterLibrary
 # Clegg et al. (1994). J. Chem. Soc., Faraday Trans. 90, 1875-1894,
 #  doi:10.1039/FT9949001875
 # System: H-HSO4-SO4
-CRP94 = ParameterLibrary()
-CRP94.name = 'CRP94'
-CRP94.dh['Aosm'] = debyehueckel.Aosm_CRP94
-CRP94.bC['H-HSO4'] = prm.bC_H_HSO4_CRP94
-CRP94.bC['H-SO4' ] = prm.bC_H_SO4_CRP94
-CRP94.theta['HSO4-SO4'] = prm.theta_HSO4_SO4_CRP94
-CRP94.psi['H-HSO4-SO4'] = prm.psi_H_HSO4_SO4_CRP94
-CRP94.jfunc = unsymmetrical.P75_eq47
-CRP94.get_contents()
+name = 'CRP94'
+dh = {'Aosm': debyehueckel.Aosm_CRP94}
+bC = {}
+bC['H-HSO4'] = prm.bC_H_HSO4_CRP94
+bC['H-SO4' ] = prm.bC_H_SO4_CRP94
+theta = {'HSO4-SO4': prm.theta_HSO4_SO4_CRP94}
+psi = {'H-HSO4-SO4': prm.psi_H_HSO4_SO4_CRP94}
+jfunc = unsymmetrical.P75_eq47
