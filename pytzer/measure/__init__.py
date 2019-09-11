e4ee5fdea4b4e97312c7ffa8539ecf301a7f501d
Matthew Humphreys <mvdh7@users.noreply.github.com>
2019-09-11 11:33:43 +0100

    Tidy up for release

--- pytzer/measure/__init__.py
+++
@@ -1,6 +0,0 @@
-# Pytzer: Pitzer model for chemical activities in aqueous solutions.
-# Copyright (C) 2019  Matthew Paul Humphreys  (GNU GPLv3)
-"""Tools for analysing experimental measurements."""
-from . import isopiestic
-
-__all__ = ['isopiestic',]
