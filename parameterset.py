bc9c40ddb0d331a2a8a0ba52b767230b19ca1e12
Matthew Humphreys <m.humphs@gmail.com>
2020-08-23 21:29:15 +0200

    trying stuff out

--- parameterset.py
+++ parameterset.py
@@ -1,55 +1,71 @@
+from jax import numpy as np
+
+
 def ca_none(T, P):
     """cation:anion --- no interaction effect."""
     return {
         "beta0": 0,
         "beta1": 0,
-        'alpha1': 1,
+        "alpha1": 1,
         "beta2": 0,
-        'alpha2': 1,
-        'C0': 0,
-        'C1': 0,
-        'omega': 1,
-        'valid': (T > 0) & (P > 0),
+        "alpha2": 1,
+        "C0": 0,
+        "C1": 0,
+        "omega": 1,
+        "valid": (T > 0) & (P > 0),
     }
 
 
+def g(x):
+    """g function following CRP94 Eq. (AI13)."""
+    return 2 * (1 - (1 + x) * np.exp(-x)) / x ** 2
+
+
+def h(x):
+    """h function following CRP94 Eq. (AI15)."""
+    return (6 - (6 + x * (6 + 3 * x + x ** 2)) * np.exp(-x)) / x ** 4
+
+
+def B(I, beta0, beta1, alpha1, beta2, alpha2):
+    """B function following CRP94 Eq. (AI7)."""
+    return beta0 + beta1 * g(alpha1 * np.sqrt(I)) + beta2 * g(alpha2 * np.sqrt(I))
+
+
+def CT(I, C0, C1, omega):
+    """CT function following CRP94 Eq. (AI10)."""
+    return C0 + 4 * C1 * h(omega * np.sqrt(I))
+
 
 class CationAnion:
-    
     def __init__(self, cation, anion, func=ca_none):
         self.cation = cation
         self.anion = anion
         self.func = func
-        
-        
+
     def get_parameters(self, T=298.15, P=1.01325):
         return self.func(T, P)
 
-
-    def get_BCfunc(self, I):
+    def get_BC(self, I):
         return
 
 
-class ParameterLibrary:
-    pass
+# class ParameterLibrary:
+#     pass
 
 
-
-class CationAnionInteractions:
-    
+class Interactions:
     def __init__(self, name=None):
         self.name = name
         self.interactions = {}
-        
+
     def __setattr__(self, name, value):
         super().__setattr__(name, value)
 
-
-    def add_interaction(self, cation, anion, func=ca_none):
-        self.interactions[cation + ":" + anion] = func
-
+    def add_interaction(self, interaction):
+        self.interactions[interaction.cation + ":" + interaction.anion] = interaction
 
 
 ca = CationAnion("Na", "Cl")
 
-test = CationAnionInteractions(name="trying")
+test = Interactions()
+test.add_interaction(ca)
--- pytzerjax.py
+++ pytzerjax.py
@@ -1,25 +1,89 @@
 import jax
 from jax import numpy as np
 
+
+def M88_eq13(T, a):
+    """M88 equation 13."""
+    return (
+        a[0]
+        + a[1] * T
+        + a[2] / T
+        + a[3] * np.log(T)
+        + a[4] / (T - 263)
+        + a[5] * T ** 2
+        + a[6] / (680 - T)
+        + a[7] / (T - 227)
+    )
+
+
+def Aosm_M88(T, P):
+    """From Moller (1988)."""
+    Aosm = M88_eq13(
+        T,
+        np.array(
+            [
+                3.36901532e-1,
+                -6.32100430e-4,
+                9.14252359e00,
+                -1.35143986e-2,
+                2.26089488e-3,
+                1.92118597e-6,
+                4.52586464e1,
+                0,
+            ]
+        ),
+    )
+    valid = (T >= 273.15) & (T <= 573.15) & (P == 10.1325)
+    return Aosm, valid
+
+
+class ParameterLibrary:
+    def __init__(self, T):
+        self.Aosm = Aosm_M88(T, 10.1325)
+
+
+pl = ParameterLibrary(298.15)
+
+
 @jax.jit  # jit here speeds this function by 4x and its deriv by 2x
 def ionic_strength(molalities, charges):
     """Ionic strength."""
     return 0.5 * np.sum(molalities * charges ** 2)
+
 
 def ionic_z(molalities, charges):
     """Z function."""
     return np.sum(molalities * np.abs(charges))
 
 
-molalities = np.array([1., 2., 3., 2., 1.])
+molalities = np.array([1.0, 2.0, 3.0, 2.0, 1.0])
 charges = np.array([2, -1, 1, -1, -1])
 
 # def excess_Gibbs(molalities, temperature=298.15, pressure=1.01325):
-    
-    
+
+
+@jax.jit
+def Gibbs_DH(I, Aosm, b=1.2):  # from CRP94 Eq. (AI1)
+    """Calculate the Debye-Hueckel component of the excess Gibbs energy."""
+    return -4 * Aosm * I * np.log(1 + b * np.sqrt(I)) / b
+
+
+@jax.jit
+def Gibbs(molalities, charges, prmlib):
+    I = ionic_strength(molalities, charges)
+    return Gibbs_DH(I, prmlib["Aosm"])
+
+
+@jax.jit
+def activity_coefficients(molalities, charges, prmlib):
+    return jax.grad(Gibbs)(molalities, charges, prmlib)
+
+
+plibs = {"Aosm": 3.1}
 istr = ionic_strength(molalities, charges)
 zstr = ionic_z(molalities, charges)
-
+g = Gibbs(molalities, charges, plibs)
+acf = activity_coefficients(molalities, charges, plibs)
 
 istr_grad = jax.jit(jax.grad(ionic_strength))
 # ^ 1000 x faster with jit here, regardless of whether jit above is there
@@ -38,7 +102,8 @@ def sum_loop(cats, anis, combi):
         for a, ani in enumerate(anis.ravel()):
             total = total + cat * ani * combi[c, a]
     return total
-    
+
+
 @jax.jit
 def sum_matrix(cats, anis, combi):
     return cats @ combi @ anis
