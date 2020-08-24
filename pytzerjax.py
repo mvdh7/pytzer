9f311d7302b04d3843b493a4c536534f5d188610
Matthew Humphreys <m.humphs@gmail.com>
2020-08-24 17:43:37 +0200

    Update pytzerjax.py

--- pytzerjax.py
+++ pytzerjax.py
@@ -1,14 +1,29 @@
 import jax
-from jax import numpy as np
+from jax import numpy as jnp
+import numpy as np
 
+def ca_none(T, P):
+    """cation:anion --- no interaction effect."""
+    return {
+        "beta0": 0,
+        "beta1": 0,
+        "alpha1": 1,
+        "beta2": 0,
+        "alpha2": 1,
+        "C0": 0,
+        "C1": 0,
+        "omega": 1,
+        "valid": (T > 0) & (P > 0),
+    }
 
+@jax.jit
 def M88_eq13(T, a):
     """M88 equation 13."""
     return (
         a[0]
         + a[1] * T
         + a[2] / T
-        + a[3] * np.log(T)
+        + a[3] * jnp.log(T)
         + a[4] / (T - 263)
         + a[5] * T ** 2
         + a[6] / (680 - T)
@@ -16,74 +31,118 @@ def M88_eq13(T, a):
     )
 
 
-def Aosm_M88(T, P):
+@jax.jit
+def Aphi_M88(T, P):
     """From Moller (1988)."""
-    Aosm = M88_eq13(
+    Aphi = M88_eq13(
         T,
-        np.array(
-            [
-                3.36901532e-1,
-                -6.32100430e-4,
-                9.14252359e00,
-                -1.35143986e-2,
-                2.26089488e-3,
-                1.92118597e-6,
-                4.52586464e1,
-                0,
-            ]
-        ),
+        [
+            3.36901532e-1,
+            -6.32100430e-4,
+            9.14252359,
+            -1.35143986e-2,
+            2.26089488e-3,
+            1.92118597e-6,
+            4.52586464e1,
+            0,
+        ],
     )
     valid = (T >= 273.15) & (T <= 573.15) & (P == 10.1325)
-    return Aosm, valid
+    return Aphi, valid
+
+
+
+
+ions2charges = {
+    "Na^+": 1,
+    "K^+": 1,
+    "Ca^{2+}": 2,
+    "Mg^{2+}": 2,
+    "Cl^-": -1,
+    "Br^-": -1,
+    "HSO_4^-": -1,
+}
+
+
+def i2c(ions):
+    return np.array([ions2charges[ion] for ion in ions])
 
 
 class ParameterLibrary:
-    def __init__(self, T):
-        self.Aosm = Aosm_M88(T, 10.1325)
+    def __init__(self):
+        self.Aphi = Aphi_M88
+        self.b = 1.2
+        self.ca = {}
+        
+        
+    def add_interaction(self, cation, anion, func=ca_none):
+        self.ca[cation + ":" + anion] = func
+        
+        
+    def get_parameters(self, ions, T=298.15, P=10.1325):
+        # charges = i2c(ions)
+        # cations = ions[charges > 0]
+        # anions = ions[charges < 0]
+        return {"Aphi": self.Aphi(T, P)[0], "b": self.b}#,
+            # "ca": [self.ca[cation + ':' + anion](T, P)
+            #        for cation in cations
+            #        for anion in anions]}
 
 
-pl = ParameterLibrary(298.15)
 
 
 @jax.jit  # jit here speeds this function by 4x and its deriv by 2x
 def ionic_strength(molalities, charges):
     """Ionic strength."""
-    return 0.5 * np.sum(molalities * charges ** 2)
+    return 0.5 * jnp.sum(molalities * charges ** 2)
 
 
+@jax.jit
 def ionic_z(molalities, charges):
     """Z function."""
-    return np.sum(molalities * np.abs(charges))
-
-
-molalities = np.array([1.0, 2.0, 3.0, 2.0, 1.0])
-charges = np.array([2, -1, 1, -1, -1])
-
-# def excess_Gibbs(molalities, temperature=298.15, pressure=1.01325):
+    return jnp.sum(molalities * jnp.abs(charges))
 
 
 @jax.jit
-def Gibbs_DH(I, Aosm, b=1.2):  # from CRP94 Eq. (AI1)
+def Gibbs_DH(I, parameters):  # from CRP94 Eq. (AI1)
     """Calculate the Debye-Hueckel component of the excess Gibbs energy."""
-    return -4 * Aosm * I * np.log(1 + b * np.sqrt(I)) / b
+    b = parameters["b"]
+    return -4 * parameters["Aphi"] * I * jnp.log(1 + b * jnp.sqrt(I)) / b
 
 
 @jax.jit
-def Gibbs(molalities, charges, prmlib):
+def Gibbs(molalities, charges, parameters):
     I = ionic_strength(molalities, charges)
-    return Gibbs_DH(I, prmlib["Aosm"])
+    return Gibbs_DH(I, parameters)
 
 
 @jax.jit
-def activity_coefficients(molalities, charges, prmlib):
-    return jax.grad(Gibbs)(molalities, charges, prmlib)
+def activity_coefficients(molalities, charges, pdict):
+    return jax.grad(Gibbs)(molalities, charges, pdict)
 
 
-plibs = {"Aosm": 3.1}
-istr = ionic_strength(molalities, charges)
-zstr = ionic_z(molalities, charges)
-g = Gibbs(molalities, charges, plibs)
-acf = activity_coefficients(molalities, charges, plibs)
+def solution2mz(solution):
+    molalities = []
+    ions = []
+    for k, v in solution.items():
+        molalities.append(v)
+        ions.append(k)
+    return jnp.array(molalities), np.array(ions)
+
+
+pl = ParameterLibrary()
+solution = {
+    "Ca^{2+}": 1.0,
+    "Cl^-": 2.0,
+    "Na^+": 3.0,
+    "HSO_4^-": 2.0,
+    "Br^-": 1.0,
+}
+molalities, ions = solution2mz(solution)
+charges = jnp.array(i2c(ions))
+parameters = pl.get_parameters(ions)
+g = Gibbs(molalities, charges, parameters)
+acf = activity_coefficients(molalities, charges, parameters)
 
 istr_grad = jax.jit(jax.grad(ionic_strength))
 # ^ 1000 x faster with jit here, regardless of whether jit above is there
