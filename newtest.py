7d1629f2b90dea72732cdc24eb54666f250cc7e0
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-12-17 19:47:07 +0000

    Fix Harvie

--- newtest.py
+++ newtest.py
@@ -4,11 +4,12 @@ from jax import numpy as np
 
 x = np.sqrt(2)
 
-j__P75_eq46 = pz.unsymmetrical.P75_eq46(x)
-jg__P75_eq46 = jax.grad(pz.unsymmetrical.P75_eq46)(x)
+j__P75_eq46 = pz.unsymmetrical.P75_eq46(x).item()
+jg__P75_eq46 = jax.grad(pz.unsymmetrical.P75_eq46)(x).item()
 
-j__P75_eq47 = pz.unsymmetrical.P75_eq47(x)
-jg__P75_eq47 = jax.grad(pz.unsymmetrical.P75_eq47)(x)
+j__P75_eq47 = pz.unsymmetrical.P75_eq47(x).item()
+jg__P75_eq47 = jax.grad(pz.unsymmetrical.P75_eq47)(x).item()
 
-
-j__Harvie = pz.unsymmetrical.Harvie(x)
+j__Harvie, jg__Harvie = pz.unsymmetrical.Harvie(x)
+j__Harvie = j__Harvie.item()
+jg__Harvie = jg__Harvie.item()
