ac3d797f6da21462016d0ba3ff54d0c81eaa7310
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-12-18 12:59:05 +0000

    Add first real tests

--- README.md
+++ README.md
@@ -1,7 +1,11 @@
 # Pytzer
 
 [![pypi badge](https://img.shields.io/pypi/v/pytzer.svg?style=popout)](https://pypi.org/project/pytzer/)
-![example workflow name](https://github.com/mvdh7/pytzer/workflows/Tests/badge.svg)
+[![Docs](https://readthedocs.org/projects/pytzer/badge/?version=jax&style=flat)](https://pytzer.readthedocs.io/en/jax/)
+![Tests](https://github.com/mvdh7/pytzer/workflows/Tests/badge.svg)
+[![Coverage](https://github.com/mvdh7/pytzer/blob/jax/.misc/coverage.svg)](https://github.com/mvdh7/pytzer/blob/jax/.misc/coverage.txt)
+[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+[![Code style: black](https://img.shields.io/badge/code%20style-black-000000.svg)](https://github.com/psf/black)
 
 Pitzer model for chemical activities and other properties of aqueous solutions.  Undergoing beta testing and development.
 
--- tests/test_unsymmetrical.py
+++ tests/test_unsymmetrical.py
@@ -1,23 +1,53 @@
-import pytzer as pz
+from itertools import product
+import pytzer as pz, numpy as np
 import jax
 
-x = 2.5
-
-j__P75_eq46 = pz.unsymmetrical.P75_eq46(x).item()
-jg__P75_eq46 = jax.jit(jax.grad(pz.unsymmetrical.P75_eq46))(x).item()
-
-j__P75_eq47 = pz.unsymmetrical.P75_eq47(x).item()
-jg__P75_eq47 = jax.jit(jax.grad(pz.unsymmetrical.P75_eq47))(x).item()
-
-j__Harvie_raw, jg__Harvie_raw = [j.item() for j in pz.unsymmetrical._Harvie_raw(x)]
-
-j__Harvie = pz.unsymmetrical.Harvie(x).item()
-jg__Harvie = jax.jit(jax.grad(pz.unsymmetrical.Harvie))(x).item()
+xs = [0.5, 1.0, 2.5, 10.0]
+funcs = [pz.unsymmetrical.P75_eq46, pz.unsymmetrical.P75_eq47, pz.unsymmetrical.Harvie]
 
 
+def test_grads():
+    """Are all the functions grad-able?"""
+    for x, func in product(xs, funcs):
+        assert isinstance(jax.grad(func)(x).item(), float)
 
-def test_tests():
-    assert 1 == 1
-    
-    
-test_tests()
+
+def test_jit_grads():
+    """Are all the grad-ed functions jit-able?"""
+    for x, func in product(xs, funcs):
+        assert isinstance(jax.jit(jax.grad(func))(x).item(), float)
+
+
+def test_Harvie_values():
+    """Do Harvie and grad(Harvie) return the exact correct values?"""
+    for x in xs:
+        j__Harvie_raw, jg__Harvie_raw = [
+            j.item() for j in pz.unsymmetrical._Harvie_raw(x)
+        ]
+        j__Harvie = pz.unsymmetrical.Harvie(x)
+        jg__Harvie = jax.grad(pz.unsymmetrical.Harvie)(x)
+        assert j__Harvie_raw == j__Harvie
+        assert jg__Harvie_raw == jg__Harvie
+
+
+def test_values():
+    """Do all functions return similar values (with 1% tolerance)?"""
+    for x in xs:
+        js = np.array([func(x).item() for func in funcs])
+        js_mean = np.mean(js)
+        assert np.allclose(js, js_mean, atol=0, rtol=0.01)
+
+
+def test_grad_values():
+    """Do all grad-ed functions return similar values (with 2% tolerance)?"""
+    for x in xs:
+        jgs = np.array([jax.grad(func)(x).item() for func in funcs])
+        jgs_mean = np.mean(jgs)
+        assert np.allclose(jgs, jgs_mean, atol=0, rtol=0.02)
+
+
+# test_grads()
+# test_jit_grads()
+# test_Harvie_values()
+# test_values()
+# test_grad_values()
