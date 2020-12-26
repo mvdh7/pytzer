20360f94a02cca524432a05726e229fb5b822c8e
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-12-26 17:18:54 +0000

    Complete map model

--- pytzer/model.py
+++ pytzer/model.py
@@ -137,15 +137,16 @@ def Gibbs_nRT(
         # Add n-a interactions
         for A, m_ani in enumerate(m_anis):
             Gibbs_nRT = Gibbs_nRT + m_neu_x * m_ani * 2 * na[NX][A]
-    #     # n-n' excluding n-n
-    #     for _NY, m_neu_y in enumerate(m_neus[NX + 1 :]):
-    #         NY = _NY + NX + 1
-    #         Gibbs_nRT = Gibbs_nRT + m_neu_x * m_neu_y * 2 * nn[NX][NY]
-    #     # n-n
-    #     Gibbs_nRT = Gibbs_nRT + m_neu_x ** 2 * nn[NX][NX]
-    #     # n-n-n
-    #     Gibbs_nRT = Gibbs_nRT + m_neu_x ** 3 * nnn[NX]
+        # n-n' excluding n-n
+        for _NY, m_neu_y in enumerate(m_neus[NX + 1 :]):
+            NY = _NY + NX + 1
+            Gibbs_nRT = Gibbs_nRT + m_neu_x * m_neu_y * 2 * nn[NX][NY]
+        # n-n
+        Gibbs_nRT = Gibbs_nRT + m_neu_x ** 2 * nn[NX][NX]
+        # n-n-n
+        Gibbs_nRT = Gibbs_nRT + m_neu_x ** 3 * nnn[NX]
     return Gibbs_nRT
+
 
 @jax.jit
 def Gibbs_map(
@@ -166,36 +167,37 @@ def Gibbs_map(
     nca=None,
     nnn=None,
 ):
-
     def add_ca(i_ca):
         ic, ia = i_ca
         v_ca = ca[ic][ia]
-        return m_cats[ic] * m_anis[ia] * (
-            2 * B(sqrt_I, v_ca[0], v_ca[1], v_ca[2], v_ca[5], v_ca[6])
-            + Z * CT(sqrt_I, v_ca[3], v_ca[4], v_ca[7])
+        return (
+            m_cats[ic]
+            * m_anis[ia]
+            * (
+                2 * B(sqrt_I, v_ca[0], v_ca[1], v_ca[2], v_ca[5], v_ca[6])
+                + Z * CT(sqrt_I, v_ca[3], v_ca[4], v_ca[7])
+            )
         )
-
 
     def add_cc(i_xy):
         ix, iy = i_xy
-        return m_cats[ix] * m_cats[iy] * (cc[ix][iy] +
-            etheta(
-            Aphi, I, z_cats[ix], z_cats[iy], func_J=func_J
-        ))
-
+        return (
+            m_cats[ix]
+            * m_cats[iy]
+            * (cc[ix][iy] + etheta(Aphi, I, z_cats[ix], z_cats[iy], func_J=func_J))
+        )
 
     def add_aa(i_xy):
         ix, iy = i_xy
-        return m_anis[ix] * m_anis[iy] * (aa[ix][iy] +
-            etheta(
-            Aphi, I, z_anis[ix], z_anis[iy], func_J=func_J
-        ))
-
+        return (
+            m_anis[ix]
+            * m_anis[iy]
+            * (aa[ix][iy] + etheta(Aphi, I, z_anis[ix], z_anis[iy], func_J=func_J))
+        )
 
     def add_cca(i_cca):
         ix, iy, ia = i_cca
         return 0.5 * m_cats[ix] * m_cats[iy] * m_anis[ia] * cca[ix][iy][ia]
-
 
     def add_caa(i_caa):
         ic, ix, iy = i_caa
@@ -213,23 +215,37 @@ def Gibbs_map(
         il, ic, ia = i_nca
         return m_neus[il] * m_cats[ic] * m_anis[ia] * nca[il][ic][ia]
 
+    def add_nn(i_nn):
+        ix, iy = i_nn
+        return m_neus[ix] * m_neus[iy] * nn[ix][iy]
+
+    def add_nnn(i_nnn):
+        return m_neus[i_nnn] ** 3 * nnn[i_nnn]
+
     n_cats = len(m_cats)
     n_anis = len(m_anis)
     n_neus = len(m_neus)
     i_ca = np.array(list(itertools.product(range(n_cats), range(n_anis))))
     i_cc = np.array(list(itertools.product(range(n_cats), range(n_cats))))
     i_aa = np.array(list(itertools.product(range(n_anis), range(n_anis))))
-    i_cca = np.array(list(itertools.product(range(n_cats), range(n_cats), range(n_anis))))
-    i_caa = np.array(list(itertools.product(range(n_cats), range(n_anis), range(n_anis))))
+    i_cca = np.array(
+        list(itertools.product(range(n_cats), range(n_cats), range(n_anis)))
+    )
+    i_caa = np.array(
+        list(itertools.product(range(n_cats), range(n_anis), range(n_anis)))
+    )
     i_nc = np.array(list(itertools.product(range(n_neus), range(n_cats))))
     i_na = np.array(list(itertools.product(range(n_neus), range(n_anis))))
-    i_nca = np.array(list(itertools.product(range(n_neus), range(n_cats), range(n_anis))))
+    i_nca = np.array(
+        list(itertools.product(range(n_neus), range(n_cats), range(n_anis)))
+    )
+    i_nn = np.array(list(itertools.product(range(n_neus), range(n_neus))))
+    i_nnn = np.array(list(range(n_neus)))
 
     I = ionic_strength(m_cats, z_cats) + ionic_strength(m_anis, z_anis)
     Z = ionic_z(m_cats, z_cats) + ionic_z(m_anis, z_anis)
     sqrt_I = np.sqrt(I)
-
-    Gibbs_nRT = (
+    return (
         Gibbs_DH(Aphi, I)
         + np.sum(lax.map(add_ca, i_ca))
         + np.sum(lax.map(add_cc, i_cc))
@@ -239,9 +255,9 @@ def Gibbs_map(
         + np.sum(lax.map(add_nc, i_nc))
         + np.sum(lax.map(add_na, i_na))
         + np.sum(lax.map(add_nca, i_nca))
+        + np.sum(lax.map(add_nn, i_nn))
+        + np.sum(lax.map(add_nnn, i_nnn))
     )
-
-    return Gibbs_nRT
 
 
 @jax.jit
@@ -260,15 +276,30 @@ def log_activity_coefficients(m_cats, m_anis, m_neus, z_cats, z_anis, **paramete
 
 
 @jax.jit
+def log_activity_coeffs_cations(m_cats, m_anis, m_neus, z_cats, z_anis, **parameters):
+    return jax.grad(Gibbs_map, 0)(m_cats, m_anis, m_neus, z_cats, z_anis, **parameters)
+
+
+@jax.jit
+def log_activity_coeffs_anions(m_cats, m_anis, m_neus, z_cats, z_anis, **parameters):
+    return jax.grad(Gibbs_map, 1)(m_cats, m_anis, m_neus, z_cats, z_anis, **parameters)
+
+
+@jax.jit
+def log_activity_coeffs_neutrals(m_cats, m_anis, m_neus, z_cats, z_anis, **parameters):
+    return jax.grad(Gibbs_map, 2)(m_cats, m_anis, m_neus, z_cats, z_anis, **parameters)
+
+
+@jax.jit
 def log_activity_coefficients_map(m_cats, m_anis, m_neus, z_cats, z_anis, **parameters):
     """Calculate the natural log of the activity coefficient of all solutes."""
-    log_acf_cats = jax.grad(Gibbs_map, 0)(
+    log_acf_cats = log_activity_coeffs_cations(
         m_cats, m_anis, m_neus, z_cats, z_anis, **parameters
     )
-    log_acf_anis = jax.grad(Gibbs_map, 1)(
+    log_acf_anis = log_activity_coeffs_anions(
         m_cats, m_anis, m_neus, z_cats, z_anis, **parameters
     )
-    log_acf_neus = jax.grad(Gibbs_map, 2)(
+    log_acf_neus = log_activity_coeffs_neutrals(
         m_cats, m_anis, m_neus, z_cats, z_anis, **parameters
     )
     return log_acf_cats, log_acf_anis, log_acf_neus
