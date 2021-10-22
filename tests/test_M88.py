1ad4e90531cc9d99abb9ad3f8e6381945cbe1151
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2021-10-22 16:29:06 +0200

    Fix presumed typo in M88 Table 4

--- tests/test_M88.py
+++ tests/test_M88.py
@@ -19,14 +19,11 @@ data["a_H2O_pytzer"] = data.apply(get_activity_water, axis=1)
 
 def test_M88_activity_water():
     """Can we reproduce the values from M88 Table 4?
-
-    We presume that their Point 4 contains some typo, hence worse agreement.
+    Note that we have corrected a presumed typo in row 4 (6.590 => 6.509 for Cl,
+    based on an analysis of charge balance).
     """
     for i, row in data.iterrows():
-        if row.name == 4:
-            assert np.isclose(row["a_H2O"], row["a_H2O_pytzer"], rtol=0, atol=0.01)
-        else:
-            assert np.isclose(row["a_H2O"], row["a_H2O_pytzer"], rtol=0, atol=0.0001)
+        assert np.isclose(row["a_H2O"], row["a_H2O_pytzer"], rtol=0, atol=0.0001)
 
 
 # test_M88_activity_water()
