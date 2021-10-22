93e9c318d3f84d65869923914f30852bd7d80a77
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2021-10-22 15:50:13 +0200

    Testing

--- tests/test_jaxscan.py
+++ tests/test_jaxscan.py
@@ -2,22 +2,26 @@ import itertools
 from jax import numpy as np, vmap, jit, lax
 from time import time
 
-cats = np.vstack([1., 2, 3, 4, 5])
-anis = np.array([[5., 2, 6]])
-ca = np.array([[3., 1, 4, 7, 5],
-               [3, 2, 6, 8, 4],
-               [1, 4, 3, 6, 8]])
+cats = np.vstack([1.0, 2, 3, 4, 5])
+anis = np.array([[5.0, 2, 6]])
+ca = np.array([[3.0, 1, 4, 7, 5], [3, 2, 6, 8, 4], [1, 4, 3, 6, 8]])
 
 m_cats = cats.ravel()
 m_anis = anis.ravel()
 
 go = time()
+
+
 @jit
 def test_matmul():
-    anisanis = np.array([(np.transpose(anis) @ anis)
-            [np.triu_indices(len(anis[0]), k=1)]])
-    catscats = np.array([(np.transpose(cats) @ cats)
-            [np.triu_indices(len(cats[0]), k=1)]])
+    anisanis = np.array(
+        [(np.transpose(anis) @ anis)[np.triu_indices(np.size(anis), k=1)]]
+    )
+    # anix = np.triu_indices(np.size(anis), k=1)
+    # anisanis = anis[0][anix[0]] * anis[0][anix[1]]
+    catscats = np.array(
+        [(np.transpose(cats) @ cats)[np.triu_indices(np.size(cats), k=1)]]
+    )
     return (anis @ ca @ cats)[0][0]
 
 
@@ -27,15 +31,15 @@ go = time()
 test_matmul()
 print(time() - go)
 
+
 def map_func(i_ca):
     ic, ia = i_ca
-    return (
-        m_cats[ic]
-        * m_anis[ia]
-        * ca[ia][ic]
-    )
-    
+    return m_cats[ic] * m_anis[ia] * ca[ia][ic]
+
+
 go = time()
+
+
 @jit
 def test_map():
     r_cats = range(len(m_cats))
