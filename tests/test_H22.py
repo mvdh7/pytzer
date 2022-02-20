ce99a2de04842fcd1a0e58d28b979c791b70409a
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2022-02-20 09:54:45 +0100

    Update

--- tests/test_H22.py
+++ tests/test_H22.py
@@ -4,15 +4,16 @@ import pytzer as pz, pytzer4 as pz4
 prmlib = pz.libraries.Waters13_Clegg22
 
 totals = pz.odict()
-totals["Na"] = 0.4861818
+totals["Na"] = 0.4861818 #- 0.04
 totals["Mg"] = 0.0547402
 totals["Ca"] = 0.01075004
 totals["K"] = 0.01058004
-totals["Cl"] = 0.5692021
+totals["Cl"] = 0.5692021 #+ 0.04
 totals["SO4"] = 0.02927011
 # totals["tris"] = 0.04
 
 solutes, pks = pz.solve(totals, library=prmlib, temperature=278.15)
+pH = -np.log10(solutes["H"])
 params = prmlib.get_parameters(solutes, temperature=278.15, verbose=False)
 acfs = pz.activity_coefficients(solutes, **params)
 aw = pz.activity_water(solutes, **params)
@@ -22,7 +23,7 @@ waterk_paper = 8.2481e-8 * 5.0620e-8 * 0.76188 * 0.57223 / 0.98142
 waterk_official = np.exp(prmlib["equilibria"]["H2O"](278.15))
 
 # trisk = (solutes["tris"] * solutes["H"] / solutes["trisH"]).item()
-# trisk_official = np.exp(prmlib["equilibria"]["trisH"](278.15))
+trisk_official = np.exp(prmlib["equilibria"]["trisH"](278.15))
 
 sulfk = (
     solutes["H"]
@@ -64,7 +65,7 @@ params = prmlib.get_parameters(solutes_paper, temperature=273.15, verbose=False)
 osm_paper = pz.osmotic_coefficient(solutes_paper, **params)
 aw_paper = pz.activity_water(solutes_paper, **params)
 acfs_paper = pz.activity_coefficients(solutes_paper, **params)
-print(osm_paper, aw_paper)
+# print(osm_paper, aw_paper)
 
 mols = np.vstack([v for v in solutes_paper.values()])
 ions = np.array([k for k in solutes_paper.keys()])
@@ -75,5 +76,5 @@ acfs_paper_v4 = pz4.model.acfs(mols, ions, 273.15, 10.10325, prmlib=prmlib_v4).r
 Gex_v4 = pz4.model.Gex_nRT(mols, ions, 273.15, 10.10325, prmlib=prmlib_v4)[0]
 Gex_v5 = pz.Gibbs_nRT(solutes_paper, **params).item()
 
-for m, i in zip(acfs_paper_v4, ions):
-    print("{}: {} vs {}".format(i, acfs_paper[i], m))
+# for m, i in zip(acfs_paper_v4, ions):
+#     print("{}: {} vs {}".format(i, acfs_paper[i], m))
