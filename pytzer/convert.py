a027118ac721dfe50626d21ba2bddf0afc8561c6
Matthew Humphreys <mvdh7@users.noreply.github.com>
2024-12-05 21:28:01 +0100

    Ruff-style

--- pytzer/convert.py
+++ pytzer/convert.py
@@ -1,7 +1,9 @@
 # Pytzer: Pitzer model for chemical activities in aqueous solutions.
 # Copyright (C) 2019--2024  M.P. Humphreys  (GNU GPLv3)
 from collections import OrderedDict
+
 from jax import numpy as np
+
 from . import constants, properties
 
 
